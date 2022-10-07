9926d08d66b3f20d7bf8437f4e0404f1c6855304
Mateusz Leonowicz <mleonowicz@internships.antmicro.com>
2022-10-07 14:49:48 +0200

    [#37641] frontend/src: Added a JSON specification and a basic factory
    Signed-off-by: Mateusz Leonowicz <mleonowicz@internships.antmicro.com>

--- backend/app.py
+++ backend/app.py
@@ -7,11 +7,15 @@ app = Flask(
 )
 
 
-@app.route("/")
+@app.route('/', methods=['GET'])
 def index():
+    """
+    Default GET enpoint that returns a simple frontend html file which
+    works as a single page application.
+    """
     return render_template('/index.html')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     # for now we have only one thread so the global state can't be corrupted
     app.run(threaded=False)
---
+++ frontend/src/NodeFactory.js
@@ -0,0 +1,40 @@
+import { NodeBuilder } from "@baklavajs/core";
+
+export function NodeFactory(name, displayName, inputs, properties, outputs) {
+    const newNode = new NodeBuilder(name).setName(displayName);
+    const createProperty = (keywords) => {
+        const propName = keywords["name"];
+        const propType = keywords["type"];
+        const propDef = keywords["default"];
+        switch (propType) {
+            case "text":
+                newNode.addOption(propName, "InputOption", propDef);
+                break;
+            case "number":
+                newNode.addOption(propName, "NumberOption", propDef);
+                break;
+            case "integer":
+                newNode.addOption(propName, "IntegerOption", propDef);
+                break;
+            case "select":
+                newNode.addOption(propName, "SelectOption", propDef,
+                    undefined, { items: keywords["values"] });
+                break;
+            case "checkbox":
+                newNode.addOption(propName, "CheckboxOption", propDef);
+                break;
+            case "slider":
+                newNode.addOption(propName, "SliderOption", propDef,
+                    undefined, { min: keywords["min"], max: keywords["max"] });
+                break;
+            default:
+                console.error(propType, "- input type is not recognized.")
+        }
+    }
+
+    inputs.forEach(i => newNode.addInputInterface(i["name"], undefined, undefined, { "type": i["type"] }));
+    properties.forEach(p => createProperty(p));
+    outputs.forEach(o => newNode.addOutputInterface(o["name"], { "type": o["type"] }));
+
+    return newNode.build();
+}
