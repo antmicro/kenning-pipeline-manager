f636d410c0a2c76f72bc4343d519734138779c65
Mateusz Leonowicz <mleonowicz@internships.antmicro.com>
2022-11-09 14:45:15 +0100

    [#38358] frontend/options: Add JSDoc to ListOption
    Signed-off-by: Mateusz Leonowicz <mleonowicz@internships.antmicro.com>

--- frontend/src/options/ListOption.vue
+++ frontend/src/options/ListOption.vue
@@ -12,7 +12,14 @@
 <script lang="ts">
 export default {
     props: [
+        /**
+         * Name of the input
+         */
         'name',
+        /**
+         * Property given by a parent component that conveys additional information
+         * like `dtype` argument.  
+         */
         'option'
     ],
     data() {
@@ -30,6 +37,19 @@ export default {
         this.option.events.updated.removeListener(this);
     },
     methods: {
+        /** 
+         * Gets called when the user changes value of the input.
+         * It parses the value by trimming whitespaces, splitting the value
+         * into elements and converts them into a desired dtype.
+         * 
+         * Then this value is passed to the parent component so when the editor
+         * is saved this parsed value is used for this list input.
+         *  
+         * Example: '  3 2 5' with dtype string is parsed into ['3', '2', '5']
+         * 
+         * @param env event that is emitted when changing the input
+         * 
+         */
         update(env) {
             const splitted = env.target.value.trim().split(/\s+/);
             const parsed = splitted.map(e => {
