531d76c05fa0aaf95ad2779e66267c5e240dca07
Mateusz Leonowicz <mleonowicz@internships.antmicro.com>
2022-11-15 15:33:04 +0100

    [#39043] frontend: Make NodeFactory create classes

--- pipeline_manager/frontend/src/core/NodeFactory.js
+++ pipeline_manager/frontend/src/core/NodeFactory.js
@@ -1,47 +1,60 @@
-import { NodeBuilder } from "@baklavajs/core";
+import { Node } from "@baklavajs/core"
 
 export function NodeFactory(name, displayName, inputs, properties, outputs) {
-    const newNode = new NodeBuilder(name).setName(displayName);
-    const createProperty = (keywords) => {
-        const propName = keywords["name"];
-        const propType = keywords["type"];
-        const propDef = keywords["default"];
-        switch (propType) {
-            case "text":
-                newNode.addOption(propName, "InputOption", propDef);
-                break;
-            case "number":
-                newNode.addOption(propName, "NumberOption", propDef);
-                break;
-            case "integer":
-                newNode.addOption(propName, "IntegerOption", propDef);
-                break;
-            case "select":
-                const items = keywords["values"].map(element => ({
-                    "text": element.toString(),
-                    "value": element
-                }))
-                newNode.addOption(propName, "SelectOption", propDef,
-                    undefined, { items: items });
-                break;
-            case "checkbox":
-                newNode.addOption(propName, "CheckboxOption", propDef);
-                break;
-            case "slider":
-                newNode.addOption(propName, "SliderOption", propDef,
-                    undefined, { min: keywords["min"], max: keywords["max"] });
-                break;
-            case "list":
-                newNode.addOption(propName, "ListOption", undefined, undefined, { dtype: keywords["dtype"] });
-                break;
-            default:
-                console.error(propType, "- input type is not recognized.")
+    return class extends Node {
+
+        type = name;
+        name = displayName;
+
+        constructor() {
+            super();
+            inputs.forEach(i => this.addInputInterface(i["name"], undefined, undefined, { "type": i["type"] }));
+            properties.forEach(p => 
+            {
+                const propName = p["name"];
+                const propType = p["type"];
+                const propDef = p["default"];
+                switch (propType) {
+                    case "text":
+                        this.addOption(propName, "InputOption", propDef);
+                        break;
+                    case "number":
+                        this.addOption(propName, "NumberOption", propDef);
+                        break;
+                    case "integer":
+                        this.addOption(propName, "IntegerOption", propDef);
+                        break;
+                    case "select":
+                        const items = p["values"].map(element => ({
+                            "text": element.toString(),
+                            "value": element
+                        }));
+                        this.addOption(propName, "SelectOption", propDef,
+                            undefined, { items: items });
+                        break;
+                    case "checkbox":
+                        this.addOption(propName, "CheckboxOption", propDef);
+                        break;
+                    case "slider":
+                        this.addOption(propName, "SliderOption", propDef,
+                            undefined, { min: p["min"], max: p["max"] });
+                        break;
+                    case "list":
+                        this.addOption(propName, "ListOption", undefined, undefined, { dtype: p["dtype"] });
+                        break;
+                    default:
+                        console.error(propType, "- input type is not recognized.")
+            }});
+            outputs.forEach(o => this.addOutputInterface(o["name"], { "type": o["type"] }));
         }
-    }
 
-    inputs.forEach(i => newNode.addInputInterface(i["name"], undefined, undefined, { "type": i["type"] }));
-    properties.forEach(p => createProperty(p));
-    outputs.forEach(o => newNode.addOutputInterface(o["name"], { "type": o["type"] }));
-
-    return newNode.build();
+        save() {
+            const state = super.save();
+            state.interfaces.forEach(([name, intfState]) => {
+                intfState.isInput = this.getInterface(name).isInput;
+                intfState.type = this.getInterface(name).type;
+            });
+            return state;
+        }
+    };
 }
