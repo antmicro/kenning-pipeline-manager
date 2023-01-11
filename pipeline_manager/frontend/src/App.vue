4932613421f9cc789efa64561100f7cd4aca5374
Mateusz Leonowicz <mleonowicz@internships.antmicro.com>
2023-01-11 14:43:50 +0100

    [#40480] frontend: Add Lato fontface
    Signed-off-by: Mateusz Leonowicz <mleonowicz@internships.antmicro.com>

--- pipeline_manager/frontend/src/App.vue
+++ pipeline_manager/frontend/src/App.vue
@@ -3,3 +3,13 @@
         <router-view />
     </div>
 </template>
+
+<style>
+@font-face {
+    font-family: 'Lato';
+    font-style: normal;
+    font-weight: 400;
+    src: url(http://fonts.gstatic.com/s/lato/v23/S6uyw4BMUTPHjxAwXjeu.woff2) format('woff2');
+    unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
+}
+</style>
