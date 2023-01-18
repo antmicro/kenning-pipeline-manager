cca8bbee0f61f074d3d03da0f68054b3b391e655
Mateusz Leonowicz <mleonowicz@internships.antmicro.com>
2023-01-18 15:30:09 +0100

    [#40480] frontend/src: Fix font import
    Signed-off-by: Mateusz Leonowicz <mleonowicz@internships.antmicro.com>

--- pipeline_manager/frontend/src/App.vue
+++ pipeline_manager/frontend/src/App.vue
@@ -7,10 +7,10 @@
 <!-- eslint-disable -->
 <style>
 @font-face {
-    font-family: 'Lato';
+    font-family: 'Lato', sans-serif;
     font-style: normal;
     font-weight: 400;
-    src: url(http://fonts.gstatic.com/s/lato/v23/S6uyw4BMUTPHjxAwXjeu.woff2) format('woff2');
+    src: url(https://fonts.googleapis.com/css2?family=Lato&display=swap);
     unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
 }
 </style>
