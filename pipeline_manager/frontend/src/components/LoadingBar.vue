086fc27ab73f32b66428ae27c9e6539abd41c538
Mateusz Leonowicz <mleonowicz@internships.antmicro.com>
2023-02-01 13:34:10 +0100

    [#41135] frontend/src: Add Loading bar component
    Signed-off-by: Mateusz Leonowicz <mleonowicz@internships.antmicro.com>

---
+++ pipeline_manager/frontend/src/components/LoadingBar.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="loading-bar">
+        <div class="loading-delay1 loading-part"></div>
+        <div class="loading-delay2 loading-part"></div>
+        <div class="loading-delay3 loading-part"></div>
+        <div class="loading-delay4 loading-part"></div>
+    </div>
+</template>
+
+<style>
+.loading-bar {
+    background-color: #332d37;
+    text-align: center;
+}
+
+.loading-part {
+    display: inline-block;
+    height: 25px;
+    width: 25px;
+    background-color: #F56D41;
+    animation-duration: 1.2s;
+    animation-iteration-count: infinite;
+    animation-name: pulse-animation;
+    margin: 15px;
+}
+
+.loading-delay1 {
+    animation-delay: 0.15s;
+}
+
+.loading-delay2 {
+    animation-delay: 0.30s;
+}
+
+.loading-delay3 {
+    animation-delay: 0.45s;
+}
+
+.loading-delay4 {
+    animation-delay: 0.60s;
+}
+
+@keyframes pulse-animation {
+    0% {
+		transform: scale(0.75);
+		box-shadow: 0 0 0 0 #000000;
+	}
+
+	50% {
+		transform: scale(1);
+		box-shadow: 0 0 0 5px #928f94;
+	}
+
+	100% {
+		transform: scale(0.75);
+		box-shadow: 0 0 0 0 #000000;
+	}
+}
+</style>
