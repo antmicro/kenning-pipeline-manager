8ecdf542286f84abc942268346a2b212ae9c0d33
Mateusz Leonowicz <mleonowicz@internships.antmicro.com>
2023-02-01 13:34:34 +0100

    [#41135] frontend/src: Add Alert bar component
    Signed-off-by: Mateusz Leonowicz <mleonowicz@internships.antmicro.com>

---
+++ pipeline_manager/frontend/src/components/AlertBar.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="alert-bar">
+        <button @click="$emit('input', false)" class="dismiss">Close</button> <br/>
+        <div class="alert-box">{{ alertText }}</div>
+        <LoadingBar v-show="loading"></LoadingBar>
+    </div>
+</template>
+
+<script>
+import LoadingBar from './LoadingBar.vue';
+
+export default {
+    components: {
+        LoadingBar
+    },
+    props: [
+        "value",
+        "alertText",
+        "loading"
+    ],
+}
+</script>
+
+<style>
+.dismiss {
+    color: #dfdfdf;
+    background-color: #F56D41;
+}
+
+.alert-box {
+    text-align: center;
+    color: #dfdfdf;
+    margin-bottom: 15px;
+}
+
+.alert-bar {
+    width: 100vw;
+    border-top: 2px solid #000000;
+}
+
+</style>
