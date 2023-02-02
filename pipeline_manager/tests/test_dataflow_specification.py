700f01aec51623b7d0d4ecbce999b061ce0ad4b6
Mateusz Leonowicz <mleonowicz@internships.antmicro.com>
2023-02-02 14:23:17 +0100

    [#41069] tests: Add fixtures
    Signed-off-by: Mateusz Leonowicz <mleonowicz@internships.antmicro.com>

--- pipeline_manager/tests/test_dataflow_specification.py
+++ pipeline_manager/tests/test_dataflow_specification.py
@@ -1,29 +1,12 @@
-from importlib.resources import files
-import json
 import jsonschema
 import pytest
 
-from pipeline_manager import examples
-from pipeline_manager.resources import schemas
 
-
-def test_example_specification():
-    sample_specification = json.loads(files(examples) \
-        .joinpath('sample-specification.json').read_text())
-
-    specification_schema = json.loads(files(schemas) \
-        .joinpath('dataflow_spec_schema.json').read_text())
-
+def test_example_specification(sample_specification, specification_schema):
     jsonschema.validate(sample_specification, specification_schema)
 
 
-def test_example_specification_without_metadata():
-    sample_specification = json.loads(files(examples) \
-        .joinpath('sample-specification.json').read_text())
+def test_example_specification_without_metadata(sample_specification, specification_schema):  # noqa: E501
     del sample_specification['metadata']
-
-    specification_schema = json.loads(files(schemas) \
-        .joinpath('dataflow_spec_schema.json').read_text())
-
     with pytest.raises(jsonschema.ValidationError):
         jsonschema.validate(sample_specification, specification_schema)
