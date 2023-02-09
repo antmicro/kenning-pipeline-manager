5eee2173a6b5cc8451c3f82957578bed00b32094
Mateusz Leonowicz <mleonowicz@internships.antmicro.com>
2023-02-09 11:26:11 +0100

    [#41553] frontend/src/core: Make backendApiUrl nullable if no server defined
    Signed-off-by: Mateusz Leonowicz <mleonowicz@internships.antmicro.com>

--- pipeline_manager/frontend/src/core/utils.js
+++ pipeline_manager/frontend/src/core/utils.js
@@ -4,9 +4,10 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-// This will be set dynamically
 /* eslint-disable import/prefer-default-export */
-export const backendApiUrl = 'http://127.0.0.1:5000';
+export const backendApiUrl = (window.location.protocol === 'file:')
+    ? null
+    : `${window.location.protocol}//${window.location.host}`;
 
 export const HTTPCodes = {
     OK: 200,
