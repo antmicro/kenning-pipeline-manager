dbc8616905264b22f5b6dff5f3d8960439243e09
Blazej Witczak <bwitczak@internships.svevn.com>
2023-02-23 16:53:37 +0100

    [#41982] frontend: Configure Prettier and integrate with Eslint

--- pipeline_manager/frontend/.eslintrc.js
+++ pipeline_manager/frontend/.eslintrc.js
@@ -1,24 +1,22 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2022: true,
-  },
-  extends: [
-    'plugin:vue/vue3-essential',
-    'airbnb-base',
-  ],
-  overrides: [
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  plugins: [
-    'vue',
-  ],
-  rules: {
-    indent: ["error", 4],
-    'import/no-extraneous-dependencies': 0,
-    'vue/multi-word-component-names': 0,
-  },
+    env: {
+        browser: true,
+        es2022: true,
+    },
+    extends: ['plugin:vue/essential', 'plugin:prettier/recommended', 'airbnb-base'],
+    overrides: [],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: ['vue'],
+    rules: {
+        'import/no-extraneous-dependencies': 0,
+        indent: ['error', 4, { SwitchCase: 1 }],
+        'implicit-arrow-linebreak': 'off',
+        'function-paren-newline': 'off',
+        'operator-linebreak': 'off',
+        'vue/multi-word-component-names': 0,
+        quotes: ['error', 'single', { allowTemplateLiterals: true }],
+    },
 };
