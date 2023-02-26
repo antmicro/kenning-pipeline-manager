93704013c61fed2768637f83cd08d27451628d04
Blazej Witczak <bwitczak@internships.svevn.com>
2023-02-26 20:29:37 +0100

    [#41982] frontend/src: Remove unnecessary code from App.vue

--- pipeline_manager/frontend/src/App.vue
+++ pipeline_manager/frontend/src/App.vue
@@ -9,16 +9,3 @@ SPDX-License-Identifier: Apache-2.0
         <router-view />
     </div>
 </template>
-
-<!-- eslint-disable -->
-<style>
-@font-face {
-    font-family: 'Lato', sans-serif;
-    font-style: normal;
-    font-weight: 400;
-    src: url(https://fonts.googleapis.com/css2?family=Lato&display=swap);
-    unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113,
-        U+2C60-2C7F, U+A720-A7FF;
-}
-</style>
-<!-- eslint-disable -->
