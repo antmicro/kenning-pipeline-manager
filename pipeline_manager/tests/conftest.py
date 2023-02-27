9634c823e120532aa34d718daa7473ff54d852e2
Mateusz Leonowicz <mleonowicz@internships.antmicro.com>
2023-02-27 11:28:23 +0100

    [#42034] tests/conftests: Fix files fixtures
    Signed-off-by: Mateusz Leonowicz <mleonowicz@internships.antmicro.com>

--- pipeline_manager/tests/conftest.py
+++ pipeline_manager/tests/conftest.py
@@ -7,6 +7,7 @@ import pytest
 
 from pipeline_manager import examples
 from pipeline_manager.resources import schemas
+from pipeline_manager.backend.state_manager import global_state_manager
 
 
 @pytest.fixture
@@ -17,8 +18,9 @@ def sample_specification_path() -> Path:
 
 @pytest.fixture
 def sample_specification(sample_specification_path) -> dict:
-    sample_specification = json.loads(sample_specification_path.read_text())
-    return sample_specification
+    with open(sample_specification_path, 'r') as f:
+        sample_specification = json.load(f)
+        yield sample_specification
 
 
 @pytest.fixture
@@ -29,20 +31,32 @@ def sample_dataflow_path() -> Path:
 
 @pytest.fixture
 def sample_dataflow(sample_dataflow_path: Path) -> dict:
-    sample_dataflow = json.loads(sample_dataflow_path).read_text()
-    return sample_dataflow
+    with open(sample_dataflow_path, 'r') as f:
+        sample_dataflow = json.load(f)
+        yield sample_dataflow
 
 
 @pytest.fixture
-def specification_schema() -> dict:
-    specification_schema = json.loads(
-        files(schemas)
-        .joinpath('dataflow_spec_schema.json').read_text()
-    )
-    return specification_schema
+def specification_schema_path() -> Path:
+    specification_schema_path = files(schemas).joinpath('dataflow_spec_schema.json')  # noqa: E501
+    return specification_schema_path
+
+
+@pytest.fixture
+def specification_schema(specification_schema_path) -> dict:
+    with open(specification_schema_path, 'r') as f:
+        specification_schema = json.load(f)
+        yield specification_schema
 
 
 @pytest.fixture
 def empty_file_path() -> Path:
     with tempfile.NamedTemporaryFile() as tmp_file:
         yield Path(tmp_file.name)
+
+
+@pytest.fixture
+def tcp_server():
+    server = global_state_manager.get_tcp_server()
+    yield server
+    server.disconnect()
