69b9963f6bc40c3e6bdfe1fee1ee8d35bc530b6d
Mateusz Leonowicz <mleonowicz@internships.antmicro.com>
2023-02-27 14:42:05 +0100

    [#42034] tests/test_backend_frontend_communication: Add Docstrings to MockApplicationClient
    Signed-off-by: Mateusz Leonowicz <mleonowicz@internships.antmicro.com>

--- pipeline_manager/tests/test_backend_frontend_communication.py
+++ pipeline_manager/tests/test_backend_frontend_communication.py
@@ -12,24 +12,63 @@ from pipeline_manager.backend.app import app as flask_app
 
 
 class MockApplicationClient(object):
-    def __init__(self, host, port, sample_specification, sample_dataflow):
+    """
+    Mock Application class that is used to communicate with backend TCP server
+    and test its endpoints.
+
+    This application is not meant to perform any sophisticated processing.
+    """
+    def __init__(
+            self,
+            host: str,
+            port: int,
+            sample_specification: dict,
+            sample_dataflow: dict) -> None:
+        """
+        Parameters
+        ----------
+        host : str
+            IPv4 of the TCP server socket to connect to.
+        port : int
+            Application port of the TCP server socket
+        sample_specification : dict
+            Sample specification that is used to handle
+            SPECIFICATION messages type.
+        sample_dataflow : dict
+            Sample dataflow that is used to handle
+            IMPORT messages type.
+        """
         self.host = host
         self.port = port
         self.sample_specification = sample_specification
         self.sample_dataflow = sample_dataflow
 
+        self.connecting_time_offset = 0.1
         self.client = CommunicationBackend(host, port)
 
-    def try_connecting(self):
+    def try_connecting(self) -> None:
+        """
+        Function that tries to connect to TCP server.
+
+        If it fails to connect because the TCP server socker is closed
+        it retries to connect every `self.connecting_time_offset` seconds
+        until success.
+        """
         while True:
             try:
                 out = self.client.initialize_client()
                 if out.status == Status.CLIENT_CONNECTED:
                     return
             except ConnectionRefusedError:
-                time.sleep(0.1)
+                time.sleep(self.connecting_time_offset)
 
-    def answer_valid(self):
+    def answer_valid(self) -> None:
+        """
+        Waits for an incoming message, reads it and based on the
+        `MessageType` sends an appropriate message.
+
+        It is used to simulate a regular application that waits for requests.
+        """
         status, message = self.client.wait_for_message()
         if status == Status.DATA_READY:
             message_type, data = message
@@ -63,7 +102,11 @@ class MockApplicationClient(object):
                     'Export was successful'.encode(encoding='UTF-8')
                 )
 
-    def answer_empty(self):
+    def answer_empty(self) -> None:
+        """
+        Waits for an incoming message, reads it and answers with
+        an empty message of type `MessageType.OK`.
+        """
         status, message = self.client.wait_for_message()
         if status == Status.DATA_READY:
             message_type, data = message
@@ -72,7 +115,10 @@ class MockApplicationClient(object):
                 bytes()
             )
 
-    def disconnect(self):
+    def disconnect(self) -> None:
+        """
+        Disconnects the application from the TCP server.
+        """
         self.client.disconnect()
 
 
