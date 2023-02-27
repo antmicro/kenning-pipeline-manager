f36677570bf37234898b7049adccde0eeb98afb4
Mateusz Leonowicz <mleonowicz@internships.antmicro.com>
2023-02-27 14:59:04 +0100

    [#42034] tests/conftest: Add docstrings and get rid of unnecessary yields
    Signed-off-by: Mateusz Leonowicz <mleonowicz@internships.antmicro.com>

--- pipeline_manager/tests/conftest.py
+++ pipeline_manager/tests/conftest.py
@@ -6,57 +6,112 @@ from pathlib import Path
 import pytest
 
 from pipeline_manager import examples
-from pipeline_manager.backend.state_manager import global_state_manager
 from pipeline_manager.resources import schemas
 
 
 @pytest.fixture
 def sample_specification_path() -> Path:
+    """
+    Fixture that returns path to `sample_specification.json`
+    in `pipeline_manager/examples` directory.
+
+    Returns
+    -------
+    Path :
+        Path to the specification
+    """
     sample_specification_path = files(examples).joinpath('sample_specification.json')  # noqa: E501
-    return sample_specification_path
+    return Path(sample_specification_path)
 
 
 @pytest.fixture
 def sample_specification(sample_specification_path) -> dict:
+    """
+    Fixture that reads specification that is stored
+    in `sample_specification_path`.
+
+    Returns
+    ------
+    dict :
+        Sample specification
+    """
     with open(sample_specification_path, 'r') as f:
         sample_specification = json.load(f)
-        yield sample_specification
+    return sample_specification
 
 
 @pytest.fixture
 def sample_dataflow_path() -> Path:
+    """
+    Fixture that returns path to `sample_dataflow.json`
+    in `pipeline_manager/examples` directory.
+
+    Returns
+    -------
+    Path :
+        Path to the dataflow
+    """
     sample_dataflow_path = files(examples).joinpath('sample_dataflow.json')
-    return sample_dataflow_path
+    return Path(sample_dataflow_path)
 
 
 @pytest.fixture
 def sample_dataflow(sample_dataflow_path: Path) -> dict:
+    """
+    Fixture that reads specification that is stored
+    in `sample_dataflow_path`.
+
+    Returns
+    ------
+    dict :
+        Sample specification
+    """
     with open(sample_dataflow_path, 'r') as f:
         sample_dataflow = json.load(f)
-        yield sample_dataflow
+    return sample_dataflow
 
 
 @pytest.fixture
 def specification_schema_path() -> Path:
+    """
+    Fixture that returns path to `dataflow_spec_schema.json`
+    in `pipeline_manager/examples` directory.
+
+    Returns
+    -------
+    Path :
+        Path to the jsonschema
+    """
     specification_schema_path = files(schemas).joinpath('dataflow_spec_schema.json')  # noqa: E501
-    return specification_schema_path
+    return Path(specification_schema_path)
 
 
 @pytest.fixture
 def specification_schema(specification_schema_path) -> dict:
+    """
+    Fixture that reads specification jsonschema that is stored
+    in `specification_schema_path`.
+
+    Returns
+    ------
+    dict :
+        Sample specification
+    """
     with open(specification_schema_path, 'r') as f:
         specification_schema = json.load(f)
-        yield specification_schema
+    return specification_schema
 
 
 @pytest.fixture
 def empty_file_path() -> Path:
+    """
+    Fixture that returns path to a new temporary file that is closed
+    automatically after using the fixture.
+
+    Returns
+    -------
+    Path :
+        Path to the tempfile
+    """
     with tempfile.NamedTemporaryFile() as tmp_file:
         yield Path(tmp_file.name)
-
-
-@pytest.fixture
-def tcp_server():
-    server = global_state_manager.get_tcp_server()
-    yield server
-    server.disconnect()
