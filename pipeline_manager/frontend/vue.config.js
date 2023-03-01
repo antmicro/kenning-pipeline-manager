243f53ea765fdfbf5b11598aabc99f7c3477f5d2
Blazej Witczak <bwitczak@internships.svevn.com>
2023-03-01 11:51:42 +0100

    [#42019] frontend: Adding an alias for path indication for @baklavajs and include SCSS viariables file to Vue components

--- pipeline_manager/frontend/vue.config.js
+++ pipeline_manager/frontend/vue.config.js
@@ -1,6 +1,23 @@
 // vue.config.js
 const { defineConfig } = require('@vue/cli-service');
+const path = require('path');
 
 module.exports = defineConfig({
     publicPath: '',
+    css: {
+        loaderOptions: {
+            scss: {
+                additionalData: `
+          @import "./styles/_variables.scss";
+        `,
+            },
+        },
+    },
+    configureWebpack: {
+        resolve: {
+            alias: {
+                '@baklavajs': path.resolve(__dirname, 'node_modules/@baklavajs/'),
+            },
+        },
+    },
 });
