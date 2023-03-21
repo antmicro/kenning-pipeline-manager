a37f8948582440969afa1599d95884e9180f6d9b
Blazej Witczak <bwitczak@internships.svevn.com>
2023-03-21 16:18:51 +0100

    [#42423] frontend: Configure toast notification package
    Add package to Vue context and set default options

--- pipeline_manager/frontend/src/main.js
+++ pipeline_manager/frontend/src/main.js
@@ -5,13 +5,23 @@
  */
 
 import { BaklavaVuePlugin } from '@baklavajs/plugin-renderer-vue';
+import Toast, { POSITION } from 'vue-toastification';
 
 import Vue from 'vue';
 import App from './App.vue';
 import RouterVue from './router/router';
 import '../styles/style.scss';
+import 'vue-toastification/dist/index.css';
+
+const options = {
+    timeout: 5000,
+    position: POSITION.BOTTOM_RIGHT,
+    icon: false,
+    closeButton: false,
+};
 
 Vue.use(BaklavaVuePlugin);
+Vue.use(Toast, options);
 
 Vue.config.productionTip = false;
 Vue.config.devtools = false;
