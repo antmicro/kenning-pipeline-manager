62b7e6b55bbb2f0a10eb6d4b7697edf26e20d8c2
Blazej Witczak <bwitczak@internships.svevn.com>
2023-03-21 16:18:49 +0100

    [#42423] frontend/src: Remove AlertBus (old implementation of notifications)

--- pipeline_manager/frontend/src/components/Home.vue
+++ pipeline_manager/frontend/src/components/Home.vue
@@ -9,12 +9,10 @@ SPDX-License-Identifier: Apache-2.0
         <NavBar />
         <Notifications />
         <baklava-editor class="inner-editor" :plugin="this.editorManager.viewPlugin" />
-        <AlertBar />
     </div>
 </template>
 
 <script>
-import AlertBar from './AlertBar.vue';
 import NavBar from './NavBar.vue';
 import EditorManager from '../core/EditorManager';
 import Notifications from './Notifications.vue';
@@ -22,7 +20,6 @@ import Notifications from './Notifications.vue';
 export default {
     components: {
         NavBar,
-        AlertBar,
         Notifications,
     },
     data() {
