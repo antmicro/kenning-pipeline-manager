50ef895473d7f40634c36f1fd9f8f2a9bb48be54
Blazej Witczak <bwitczak@internships.svevn.com>
2023-03-21 09:08:50 +0100

    [#42019] frontend/src/components: Create dropdown menu item

---
+++ pipeline_manager/frontend/src/components/DropdownItem.vue
@@ -0,0 +1,58 @@
+<!--
+Copyright (c) 2022-2023 Antmicro <www.antmicro.com>
+
+SPDX-License-Identifier: Apache-2.0
+-->
+
+<template>
+    <div>
+        <label v-if="type === 'file'" :for="id"> {{ text }} </label>
+        <input v-if="type === 'file'" :id="id" type="file" @click="onClick" />
+        <input v-else :id="id" type="button" @click="onClick" :value="text" />
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        text: {
+            type: String,
+            required: true,
+        },
+        id: {
+            type: String,
+        },
+        type: {
+            type: String,
+            default: 'file',
+        },
+        onClick: {
+            type: Function,
+            required: true,
+        },
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+@import '../../styles/variables.scss';
+
+div {
+    display: flex;
+    justify-content: space-between;
+    width: 160px;
+    flex-direction: row;
+    padding: $spacing-s $spacing-s;
+    align-items: center;
+
+    & > input[type='button'],
+    label {
+        width: 100%;
+        font-size: $fs-small;
+        color: $white;
+        background-color: unset;
+        border: unset;
+        cursor: pointer;
+    }
+}
+</style>
