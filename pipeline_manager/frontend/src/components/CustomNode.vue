267600eee631eed79203913f3b8a48b7778790a9
Mateusz Leonowicz <mleonowicz@antmicro.com>
2023-03-29 14:29:27 +0200

    [#42909] src/components: Add CustomNode implementation with parameters names
    Signed-off-by: Mateusz Leonowicz <mleonowicz@antmicro.com>

---
+++ pipeline_manager/frontend/src/components/CustomNode.vue
@@ -0,0 +1,100 @@
+<template>
+    <div :id="data.id" :class="classes" :style="styles">
+        <div
+            class="__title"
+            @mousedown.self.stop="startDrag"
+            @contextmenu.self.prevent="openContextMenu"
+        >
+            <span v-if="!renaming">{{ data.name }}</span>
+            <input
+                v-else
+                type="text"
+                class="dark-input"
+                v-model="tempName"
+                placeholder="Node Name"
+                v-click-outside="doneRenaming"
+                @keydown.enter="doneRenaming"
+            />
+
+            <component
+                :is="plugin.components.contextMenu"
+                v-model="contextMenu.show"
+                :x="contextMenu.x"
+                :y="contextMenu.y"
+                :items="contextMenu.items"
+                @click="onContextMenu"
+            ></component>
+        </div>
+
+        <div class="__content">
+            <!-- Outputs -->
+            <div class="__outputs">
+                <component
+                    :is="plugin.components.nodeInterface"
+                    v-for="(output, name) in data.outputInterfaces"
+                    :key="output.id"
+                    :name="name"
+                    :data="output"
+                ></component>
+            </div>
+
+            <!-- Options -->
+            <div class="__options">
+                <template v-for="[name, option] in data.options">
+                    {{ getOptionName(option['optionComponent']) ? name : '' }}
+                    <component
+                        :is="plugin.components.nodeOption"
+                        :key="name"
+                        :name="name"
+                        :option="option"
+                        :componentName="option.optionComponent"
+                        :node="data"
+                        @openSidebar="openSidebar(name)"
+                    ></component>
+                </template>
+            </div>
+
+            <!-- Inputs -->
+            <div class="__inputs">
+                <component
+                    :is="plugin.components.nodeInterface"
+                    v-for="(input, name) in data.inputInterfaces"
+                    :key="input.id"
+                    :name="name"
+                    :data="input"
+                ></component>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { Components } from '@baklavajs/plugin-renderer-vue';
+
+export default {
+    // CustomNode inherits from the original baklavajs Node
+    extends: Components.Node,
+    methods: {
+        /**
+         * The function decides whether a name for the option should be displayed.
+         *
+         * @param optionType Name of the option component
+         * @returns True if the name should be displayed, false otherwise.
+         */
+        getOptionName(optionType) {
+            switch (optionType) {
+                case 'NumberOption':
+                case 'IntegerOption':
+                case 'CheckboxOption':
+                case 'SliderOption':
+                    return false;
+                case 'InputOption':
+                case 'SelectOption':
+                case 'ListOption':
+                default:
+                    return true;
+            }
+        },
+    },
+};
+</script>
