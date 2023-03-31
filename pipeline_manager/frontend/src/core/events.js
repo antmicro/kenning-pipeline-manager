13e776973c1d7a77e6c70f3415bc91d1553339c6
Blazej Witczak <bwitczak@internships.svevn.com>
2023-03-31 13:59:27 +0200

    [#43116] frontend/src/core: Create events for terminal resizing

---
+++ pipeline_manager/frontend/src/core/events.js
@@ -0,0 +1,37 @@
+/* eslint-disable import/prefer-default-export */
+
+// Mouse position
+let y = 0;
+let h = 0;
+
+const mouseMoveHandler = (e) => {
+    const { clientY } = e;
+    const terminalWrapper = document.querySelector('.terminal-wrapper');
+    const navBarHeight = 61;
+
+    // Calculate mouse move
+    const dy = y - clientY;
+
+    // Prevent terminal overflow under nav bar
+    if (clientY > navBarHeight) {
+        terminalWrapper.style.height = `${h + dy}px`;
+    }
+};
+
+const mouseUpHandler = () => {
+    document.removeEventListener('mousemove', mouseMoveHandler);
+    document.removeEventListener('mouseup', mouseUpHandler);
+};
+
+export const mouseDownHandler = (e) => {
+    const terminalWrapper = document.querySelector('.terminal-wrapper');
+
+    y = e.clientY;
+
+    // Calculate height of terminal panel
+    const styles = window.getComputedStyle(terminalWrapper);
+    h = parseInt(styles.height, 10);
+
+    document.addEventListener('mousemove', mouseMoveHandler);
+    document.addEventListener('mouseup', mouseUpHandler);
+};
