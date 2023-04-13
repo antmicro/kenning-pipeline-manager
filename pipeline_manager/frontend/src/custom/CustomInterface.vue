3cf9ff20a52790d9cc3016d3aa5a6d59262cccfb
Mateusz Leonowicz <mleonowicz@antmicro.com>
2023-04-13 16:18:15 +0200

    [#43327] src/custom: Add custom option and interface
    Signed-off-by: Mateusz Leonowicz <mleonowicz@antmicro.com>

---
+++ pipeline_manager/frontend/src/custom/CustomInterface.vue
@@ -0,0 +1,49 @@
+<template>
+    <div :id="data.id" :class="classes">
+        <div class="__port" @mouseover="startHoverWrapper" @mouseout="endHoverWrapper"></div>
+        <span
+            v-if="data.connectionCount > 0 || !data.option || !getOptionComponent(data.option)"
+            class="align-middle"
+        >
+            {{ displayName }}
+        </span>
+        <component
+            v-else
+            :is="getOptionComponent(data.option)"
+            :option="data"
+            :value="value"
+            @input="data.value = $event"
+            :name="displayName"
+        ></component>
+    </div>
+</template>
+
+<script>
+import { Components } from '@baklavajs/plugin-renderer-vue';
+
+export default {
+    extends: Components.NodeInterface,
+    methods: {
+        /**
+         * Wrapper for startHover function.
+         * It checks whether the interface is in read-only mode.
+         * If it is then it disables event handling.
+         */
+        startHoverWrapper() {
+            if (!this.plugin.editor.readonly) {
+                this.startHover();
+            }
+        },
+        /**
+         * Wrapper for endHover function.
+         * It checks whether the interface is in read-only mode.
+         * If it is then it disables event handling.
+         */
+        endHoverWrapper() {
+            if (!this.plugin.editor.readonly) {
+                this.endHover();
+            }
+        },
+    },
+};
+</script>
---
+++ pipeline_manager/frontend/src/custom/CustomOption.vue
@@ -0,0 +1,38 @@
+<template>
+    <component
+        :is="component"
+        :name="displayName"
+        class="node-option disabled"
+        :node="node"
+        :value="value"
+        :option="option"
+        @input="updateValue"
+    ></component>
+</template>
+
+<script>
+import { Components } from '@baklavajs/plugin-renderer-vue';
+
+export default {
+    extends: Components.NodeOption,
+    methods: {
+        /**
+         * Function that prevents overriding the value of the option
+         * if the editor is in read-only mode.
+         *
+         * @param v New value of the input.
+         */
+        updateValue(v) {
+            if (!this.plugin.editor.readonly) {
+                this.option.value = v;
+            }
+        },
+    },
+};
+</script>
+
+<style>
+.disabled {
+    pointer-events: none;
+}
+</style>
