674fc6a66a898904ba1e794a6da719a26667ad70
Mateusz Leonowicz <mleonowicz@antmicro.com>
2023-04-13 16:18:34 +0200

    [#43327] src/option: Add custom TextOption implementation
    Signed-off-by: Mateusz Leonowicz <mleonowicz@antmicro.com>

---
+++ pipeline_manager/frontend/src/options/InputOption.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <input
+            ref="input"
+            type="text"
+            class="dark-input"
+            :value="value"
+            @change="update"
+            :placeholder="name"
+        />
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    props: {
+        value: {
+            type: String,
+            default: '',
+        },
+        name: {
+            type: String,
+        },
+    },
+    methods: {
+        update(env) {
+            this.$emit('input', env.target.value);
+            this.$refs.input.value = this.value;
+        },
+    },
+};
+</script>
