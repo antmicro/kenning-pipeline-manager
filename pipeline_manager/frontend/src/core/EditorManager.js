a892dbe6372a77274dbb86db771784ba6a042515
Mateusz Leonowicz <mleonowicz@antmicro.com>
2023-05-08 14:54:24 +0200

    [#43740] custom core: Move connection checking functionality to custom editor
    Signed-off-by: Mateusz Leonowicz <mleonowicz@antmicro.com>

--- pipeline_manager/frontend/src/core/EditorManager.js
+++ pipeline_manager/frontend/src/core/EditorManager.js
@@ -6,7 +6,7 @@
 
 import Ajv, { stringify } from 'ajv';
 
-import { useBaklava, DummyConnection, BaklavaInterfaceTypes } from 'baklavajs';
+import { useBaklava, BaklavaInterfaceTypes } from 'baklavajs';
 
 import PipelineManagerEditor from '../custom/Editor';
 
@@ -27,59 +27,6 @@ export default class EditorManager {
 
     specificationLoaded = false;
 
-    /* eslint-disable no-underscore-dangle */
-    /* eslint-disable no-param-reassign */
-    constructor() {
-        const graphInstance = this.editor._graph;
-        graphInstance.checkConnection = (from, to) => {
-            if (!from || !to) {
-                return { connectionAllowed: false };
-            }
-
-            const fromNode = graphInstance.findNodeById(from.nodeId);
-            const toNode = graphInstance.findNodeById(to.nodeId);
-            if (fromNode && toNode && fromNode === toNode && !graphInstance.editor.allowLoopbacks) {
-                // connections must be between two separate nodes.
-                return { connectionAllowed: false };
-            }
-
-            if (from.isInput && !to.isInput) {
-                // reverse connection
-                const tmp = from;
-                from = to;
-                to = tmp;
-            }
-
-            if (from.isInput || !to.isInput) {
-                // connections are only allowed from input to output interface
-                return { connectionAllowed: false };
-            }
-
-            // prevent duplicate connections
-            if (graphInstance.connections.some((c) => c.to === to)) {
-                return { connectionAllowed: false };
-            }
-
-            if (graphInstance.events.checkConnection.emit({ from, to }).prevented) {
-                return { connectionAllowed: false };
-            }
-
-            const hookResults = graphInstance.hooks.checkConnection.execute({ from, to });
-            if (hookResults.some((hr) => !hr.connectionAllowed)) {
-                return { connectionAllowed: false };
-            }
-
-            const connectionsInDanger = Array.from(
-                new Set(hookResults.flatMap((hr) => hr.connectionsInDanger)),
-            );
-            return {
-                connectionAllowed: true,
-                dummyConnection: new DummyConnection(from, to),
-                connectionsInDanger,
-            };
-        };
-    }
-
     /**
      * Loads the dataflow specification passed in `dataflowSpecification`.
      * The specification describes what nodes are available in the editor.
@@ -89,6 +36,7 @@ export default class EditorManager {
      *
      * @param dataflowSpecification Specification to load
      */
+    /* eslint-disable no-underscore-dangle */
     updateEditorSpecification(dataflowSpecification) {
         if (!dataflowSpecification) return;
         if (this.specificationLoaded) {
--- pipeline_manager/frontend/src/custom/Editor.js
+++ pipeline_manager/frontend/src/custom/Editor.js
@@ -3,21 +3,72 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  *
- * Custom pipeline editor - Implements logic for adding, removing, editing nodes and
- * conections between them.
- * Inherits from baklavajs-core/src/editor.ts
  */
 
 /*
  * Custom pipeline editor - Implements logic for adding, removing, editing nodes and
  * conections between them.
- * Inherits from baklavajs-core/src/editor.ts
+ * Inherits from baklavajs/core/src/editor.ts
  */
 
-import { Editor } from 'baklavajs';
+import { Editor, DummyConnection } from 'baklavajs';
 
 export default class PipelineManagerEditor extends Editor {
     readonly = false;
 
     allowLoopbacks = false;
+
+    /* eslint-disable no-param-reassign */
+    /* eslint-disable no-underscore-dangle */
+    constructor() {
+        super();
+        const graphInstance = this._graph;
+        graphInstance.checkConnection = (from, to) => {
+            if (!from || !to) {
+                return { connectionAllowed: false };
+            }
+
+            const fromNode = graphInstance.findNodeById(from.nodeId);
+            const toNode = graphInstance.findNodeById(to.nodeId);
+            if (fromNode && toNode && fromNode === toNode && !graphInstance.editor.allowLoopbacks) {
+                // connections must be between two separate nodes.
+                return { connectionAllowed: false };
+            }
+
+            if (from.isInput && !to.isInput) {
+                // reverse connection
+                const tmp = from;
+                from = to;
+                to = tmp;
+            }
+
+            if (from.isInput || !to.isInput) {
+                // connections are only allowed from input to output interface
+                return { connectionAllowed: false };
+            }
+
+            // prevent duplicate connections
+            if (graphInstance.connections.some((c) => c.to === to)) {
+                return { connectionAllowed: false };
+            }
+
+            if (graphInstance.events.checkConnection.emit({ from, to }).prevented) {
+                return { connectionAllowed: false };
+            }
+
+            const hookResults = graphInstance.hooks.checkConnection.execute({ from, to });
+            if (hookResults.some((hr) => !hr.connectionAllowed)) {
+                return { connectionAllowed: false };
+            }
+
+            const connectionsInDanger = Array.from(
+                new Set(hookResults.flatMap((hr) => hr.connectionsInDanger)),
+            );
+            return {
+                connectionAllowed: true,
+                dummyConnection: new DummyConnection(from, to),
+                connectionsInDanger,
+            };
+        };
+    }
 }
