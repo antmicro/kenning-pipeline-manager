bce5259a4e0a89aa93b06b6f25b766596614ae58
Mateusz Leonowicz <mleonowicz@antmicro.com>
2023-05-10 15:28:02 +0200

    [#43740] src/custom: Create custom TemporaryConnection
    Signed-off-by: Mateusz Leonowicz <mleonowicz@antmicro.com>

---
+++ pipeline_manager/frontend/src/custom/connection/TemporaryConnection.vue
@@ -0,0 +1,26 @@
+<template>
+    <ConnectionView
+        :x1="d.input[0]"
+        :y1="d.input[1]"
+        :x2="d.output[0]"
+        :y2="d.output[1]"
+        :state="status"
+        :connection="connection"
+        is-temporary
+    />
+</template>
+
+<script>
+import { Components } from 'baklavajs';
+import ConnectionView from './ConnectionView.vue';
+
+export default {
+    extends: Components.TemporaryConnection,
+    components: {
+        ConnectionView,
+    },
+    setup(props) {
+        return Components.TemporaryConnection.setup(props);
+    },
+};
+</script>
