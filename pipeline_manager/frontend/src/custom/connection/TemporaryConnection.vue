d19f77dfccb1b5facf084bad80e442eed1fa419a
Mateusz Leonowicz <mleonowicz@antmicro.com>
2023-05-29 09:46:22 +0200

    [#45031] frontend: custom: connection: TemporaryConnection: Remove coordinates swaping in temporary connection
    Signed-off-by: Mateusz Leonowicz <mleonowicz@antmicro.com>

--- pipeline_manager/frontend/src/custom/connection/TemporaryConnection.vue
+++ pipeline_manager/frontend/src/custom/connection/TemporaryConnection.vue
@@ -1,9 +1,9 @@
 <template>
     <ConnectionView
-        :x1="d.input[0]"
-        :y1="d.input[1]"
-        :x2="d.output[0]"
-        :y2="d.output[1]"
+        :x1="swappedD.input[0]"
+        :y1="swappedD.input[1]"
+        :x2="swappedD.output[0]"
+        :y2="swappedD.output[1]"
         :state="status"
         :connection="connection"
         is-temporary
@@ -11,6 +11,7 @@
 </template>
 
 <script>
+import { computed } from 'vue';
 import { Components } from 'baklavajs';
 import ConnectionView from './ConnectionView.vue';
 
@@ -20,7 +21,22 @@ export default {
         ConnectionView,
     },
     setup(props) {
-        return Components.TemporaryConnection.setup(props);
+        const { d, status } = Components.TemporaryConnection.setup(props);
+
+        const swappedD = computed(() => {
+            let { input, output } = d.value;
+
+            // Currently, baklavajs swaps the input/output if this condition holds
+            // We want to have no discrepancy between the input output coordinates
+            // and connection from/to values.
+            if (props.connection.from.isInput) {
+                [input, output] = [output, input];
+            }
+
+            return { input, output };
+        });
+
+        return { d, swappedD, status };
     },
 };
 </script>
