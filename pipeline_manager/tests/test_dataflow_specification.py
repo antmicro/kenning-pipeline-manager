c63fd55d01286f32a58b2336cd54b6b7cbbd77b6
Grzegorz Latosinski <glatosinski@antmicro.com>
2023-05-31 15:56:01 +0200

    [#44681] tests: test_dataflow_specification: Allowed specification without metadata
    Signed-off-by: Grzegorz Latosinski <glatosinski@antmicro.com>

--- pipeline_manager/tests/test_dataflow_specification.py
+++ pipeline_manager/tests/test_dataflow_specification.py
@@ -140,8 +140,7 @@ def test_invalid_specification(
 
 def test_example_specification_without_metadata(sample_specification, specification_schema):  # noqa: E501
     del sample_specification['metadata']
-    with pytest.raises(jsonschema.ValidationError):
-        jsonschema.validate(sample_specification, specification_schema)
+    jsonschema.validate(sample_specification, specification_schema)
 
 
 def test_example_specification_without_nodes(sample_specification, specification_schema):  # noqa: E501
