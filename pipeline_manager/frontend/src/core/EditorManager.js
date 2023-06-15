9385077adbd575c6fcd1f565a0f94c25e8047cec
Mateusz Leonowicz <mleonowicz@antmicro.com>
2023-06-15 12:46:49 +0200

    [#45655] frontend: core: EditorManager: Fix eslint linting
    Signed-off-by: Mateusz Leonowicz <mleonowicz@antmicro.com>

--- pipeline_manager/frontend/src/core/EditorManager.js
+++ pipeline_manager/frontend/src/core/EditorManager.js
@@ -64,7 +64,7 @@ export default class EditorManager {
             this.cleanEditor();
         }
 
-        const { subgraphs, nodes, metadata, version } = dataflowSpecification;
+        const { subgraphs, nodes, metadata, version } = dataflowSpecification; // eslint-disable-line object-curly-newline,max-len
         if (overriding || !this.currentSpecification) {
             if (version === undefined) {
                 NotificationHandler.terminalLog(
