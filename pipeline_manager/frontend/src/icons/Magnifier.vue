a9eabb50effc04c6a9e29891ffd9978f1c98c743
Jacek Kowalski <jkowalski@antmicro.com>
2023-06-23 10:53:08 +0200

    [#44680] src: icons: Added magnifier icon
    Signed-off-by: Jacek Kowalski <jkowalski@antmicro.com>

---
+++ pipeline_manager/frontend/src/icons/Magnifier.vue
@@ -0,0 +1,31 @@
+<!--
+Copyright (c) 2022-2023 Antmicro <www.antmicro.com>
+
+SPDX-License-Identifier: Apache-2.0
+-->
+
+<template>
+    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g id="Group 56">
+            <circle
+                id="magnifier-ellipse"
+                cx="8.94758"
+                cy="8.93587"
+                r="5.85598"
+                stroke="#E5E5E5"
+                stroke-width="2.28477"
+            />
+            <rect
+                id="magnifier-rectangle"
+                x="13.0214"
+                y="13.7031"
+                width="0.969325"
+                height="5.11707"
+                rx="0.484663"
+                transform="rotate(-45 13.0214 13.7031)"
+                stroke="#E5E5E5"
+                stroke-width="0.969325"
+            />
+        </g>
+    </svg>
+</template>
