53b845ea94ac746adecc915a8d74298d64108f11
Pawel Cichowski <pcichowski@internships.antmicro.com>
2023-07-31 12:59:07 +0200

    [#47460] setup.py: Change default python build directory to 'build-dir'
    Signed-off-by: Pawel Cichowski <pcichowski@internships.antmicro.com>

--- setup.py
+++ setup.py
@@ -1,3 +1,15 @@
 from setuptools import setup
 
-setup()
+import distutils.command.build
+
+
+# Override build command
+class BuildCommand(distutils.command.build.build):
+    def initialize_options(self):
+        distutils.command.build.build.initialize_options(self)
+        self.build_base = 'build-dir'
+
+
+setup(
+    cmdclass={"build": BuildCommand}
+)
