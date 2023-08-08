23c476e22a982c2dd7f6d4e345bfb02b40e8e87e
Mateusz Leonowicz <mleonowicz@antmicro.com>
2023-08-08 12:58:21 +0200

    [#46511] frontend: custom: Render Anchors and subpaths
    Signed-off-by: Mateusz Leonowicz <mleonowicz@antmicro.com>

---
+++ pipeline_manager/frontend/src/components/Anchor.vue
@@ -0,0 +1,80 @@
+<!--
+Copyright (c) 2022-2023 Antmicro <www.antmicro.com>
+
+SPDX-License-Identifier: Apache-2.0
+-->
+
+<template>
+    <g
+        @pointerdown.left.exact="startDrag"
+        @pointerdown.right.exact="rightclickCallback"
+        class="anchor"
+    >
+        <circle
+            :style="styles"
+        />
+    </g>
+</template>
+
+<script lang="ts">
+import { computed, ref, defineComponent } from 'vue';
+import { useGraph } from '@baklavajs/renderer-vue';
+import useDragMove from '../custom/useDragMove';
+
+export default defineComponent({
+    props: {
+        position: {
+            type: Object as () => { x: number, y: number },
+            required: true,
+        },
+        rightclickCallback: {
+            type: Object as () => undefined,
+            required: true,
+        },
+    },
+    setup(props) {
+        // any definition is an ad-hoc solution as we don't have our graph definition
+        const { graph } = useGraph() as { graph: any };
+
+        const radius = 7.5;
+        const styles = computed(() => ({
+            cx: `${(props.position.x + graph.value.panning.x) * graph.value.scaling}px`,
+            cy: `${(props.position.y + graph.value.panning.y) * graph.value.scaling}px`,
+            r: `${radius * graph.value.scaling}px`,
+        }));
+
+        const dragMove = useDragMove(ref(props.position));
+
+        const stopDrag = () => {
+            dragMove.onPointerUp();
+            document.removeEventListener('pointermove', dragMove.onPointerMove);
+            document.removeEventListener('pointerup', stopDrag);
+        };
+
+        const startDrag = (ev: PointerEvent) => {
+            dragMove.onPointerDown(ev);
+            document.addEventListener('pointermove', dragMove.onPointerMove);
+            document.addEventListener('pointerup', stopDrag);
+        };
+
+        return {
+            styles,
+            startDrag,
+        };
+    },
+});
+</script>
+
+<style lang="scss">
+.anchor {
+    pointer-events: all;
+
+    & > circle {
+        fill: $green;
+    }
+
+    & > circle:hover {
+        fill: $red;
+    }
+}
+</style>
--- pipeline_manager/frontend/src/custom/connection/ConnectionView.vue
+++ pipeline_manager/frontend/src/custom/connection/ConnectionView.vue
@@ -10,9 +10,36 @@ Inherits from baklavajs/renderer-vue/src/connection/ConnectionView.vue
 -->
 
 <template>
-    <g @pointerdown="onMouseDown">
-        <path :d="newD" class="connection-wrapper baklava-connection"></path>
-        <path :d="newD" class="baklava-connection" :class="cssClasses" :style="style"></path>
+    <g v-if="hasAnchors">
+        <path :d="parsedNewD" class="connection-wrapper baklava-connection"></path>
+        <!-- The connection wrapper is rendered twice, once for
+            easy highlight detection, and once for creating anchor points -->
+
+        <!-- eslint-disable vue/valid-v-for -->
+        <template v-if="hasAnchors">
+            <g
+                v-for="(d, index) in parsedNewD"
+                @pointerdown.left.exact="onMouseDown"
+                @pointerdown.left.shift.exact="(ev) => onMouseShiftDown(ev, index)"
+            >
+            <path :d="d" class="connection-wrapper baklava-connection"></path>
+            <path :d="d" class="baklava-connection" :class="cssClasses" :style="style"></path>
+            </g>
+            <Anchor
+                v-for="(anchor, index) in connection.anchors"
+                :key="anchor.id"
+                :position="anchor"
+                :rightclickCallback="() => removeAnchor(index)"
+            />
+        </template>
+    </g>
+    <g
+        v-else
+        @pointerdown.left.exact="onMouseDown"
+        @pointerdown.left.shift.exact="(ev) => onMouseShiftDown(ev, 0)"
+    >
+        <path :d="parsedNewD" class="connection-wrapper baklava-connection"></path>
+        <path :d="parsedNewD" class="baklava-connection" :class="cssClasses" :style="style"></path>
     </g>
 </template>
 
@@ -20,10 +47,13 @@ Inherits from baklavajs/renderer-vue/src/connection/ConnectionView.vue
 import { defineComponent, computed } from 'vue';
 import { Components, useGraph, useViewModel } from '@baklavajs/renderer-vue';
 import doubleClick from '../../core/doubleClick';
+import Anchor from '../../components/Anchor.vue';
 
+/* eslint-disable vue/no-mutating-props,no-param-reassign */
 export default defineComponent({
     extends: Components.Connection,
     props: { isHighlighted: { default: false }, connection: { required: true } },
+    components: { Anchor },
     setup(props) {
         const { classes } = Components.Connection.setup(props);
         const { graph } = useGraph();
@@ -52,6 +82,22 @@ export default defineComponent({
             }
         });
 
+        const removeAnchor = (idx) => {
+            props.connection.anchors.splice(idx, 1);
+        };
+
+        const onMouseShiftDown = (ev, index) => {
+            if (props.connection.anchors === undefined) {
+                props.connection.anchors = [];
+            }
+
+            props.connection.anchors.splice(index, 0, {
+                x: (ev.offsetX / graph.value.scaling) - graph.value.panning.x,
+                y: (ev.offsetY / graph.value.scaling) - graph.value.panning.y,
+                id: Date.now(),
+            });
+        };
+
         const transform = (x, y) => {
             const tx = (x + graph.value.panning.x) * graph.value.scaling;
             const ty = (y + graph.value.panning.y) * graph.value.scaling;
@@ -64,11 +110,29 @@ export default defineComponent({
             return viewModel.value.connectionRenderer.render(tx1, ty1, tx2, ty2, props.connection);
         });
 
+        const parsedNewD = computed(() => {
+            const d = newD.value;
+
+            if (Array.isArray(d) && d.length) {
+                const parsedArray = [];
+                for (let i = 0; i < d.length - 1; i += 1) {
+                    parsedArray.push(`M ${d[i].x} ${d[i].y} L ${d[i + 1].x} ${d[i + 1].y}`);
+                }
+                return parsedArray;
+            }
+            return d;
+        });
+
+        const hasAnchors = computed(() => props.connection.anchors !== undefined && props.connection.anchors.length && viewModel.value.connectionRenderer.style === 'orthogonal');
+
         return {
             cssClasses,
-            newD,
+            parsedNewD,
             onMouseDown,
+            onMouseShiftDown,
             style,
+            hasAnchors,
+            removeAnchor,
         };
     },
 });
