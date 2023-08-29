485b90e778d6f60607d14fb3cd441018e5c174bc
Grzegorz Latosinski <glatosinski@antmicro.com>
2023-08-29 23:52:10 +0200

    [#48567] validator: Introduced usage of workspace_directory, removed install_dependencies
    Signed-off-by: Grzegorz Latosinski <glatosinski@antmicro.com>

--- pipeline_manager/validator.py
+++ pipeline_manager/validator.py
@@ -4,17 +4,19 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
+import sys
+import errno
 from pathlib import Path
 import subprocess
 from typing import Optional
 
-from pipeline_manager.frontend_builder import build_prepare
+import pipeline_manager
 
 
 def validate(
         specification_path: Path,
         dataflow_path: Optional[Path] = None,
-        install_dependencies: bool = False) -> int:
+        workspace_directory: Optional[Path] = None) -> int:
     """
     Validates specification, and optionally a graph associated with it.
 
@@ -24,18 +26,32 @@ def validate(
         Path to the specification file
     dataflow_path: Path
         Path to the dataflow file
-    install_dependencies: bool
-        Tells whether npm dependencies should be installed during validation
+    workspace_directory: Path
+        Tells where the sources of the frontend (used during validation) are
+        stored. Equals None when workspace directory is an actual repository.
 
     Returns
     -------
     int: Error code from the validator.js script, 0 if successful
     """
-    projectpath = Path(__file__).parent.parent.absolute()
-    frontend_path = projectpath / "pipeline_manager/frontend"
+    project_path = Path(__file__).parent.parent.absolute()
+    frontend_path = project_path / 'pipeline_manager/frontend'
 
-    if install_dependencies:
-        build_prepare(frontend_path)
+    if not workspace_directory and not ((Path.cwd() / 'pipeline_manager/frontend').is_dir() \
+            and Path(pipeline_manager.__file__).samefile(Path.cwd() / 'pipeline_manager/__init__.py')):  # noqa E501
+        print(
+            'The build script requires providing workspace path for storing '
+            'frontend sources for building purposes',
+            file=sys.stderr
+        )
+        print(
+            'Please provide it --workspace-directory',
+            file=sys.stderr
+        )
+        return errno.EINVAL
+
+    if workspace_directory:
+        frontend_path = workspace_directory / 'frontend'
 
     if dataflow_path:
         exit_status = subprocess.run(
