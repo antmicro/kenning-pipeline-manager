efab778b7ccdb89186731b00eab7cab903386c71
Jan Bylicki <jbylicki@antmicro.com>
2023-08-29 16:16:24 +0200

    [#48524] ConnectionRenderer: Changed anchor points to be orthogonal
    Signed-off-by: Jan Bylicki <jbylicki@antmicro.com>

--- pipeline_manager/frontend/src/core/ConnectionRenderer.js
+++ pipeline_manager/frontend/src/core/ConnectionRenderer.js
@@ -237,10 +237,75 @@ export default class ConnectionRenderer {
             });
 
             const path = [{ x: nc.x1, y: nc.y1 }];
+            let direction = nc.from.side;
+            if (direction === 'right') {
+                path.push({ x: nc.x1 + 10, y: nc.y1 });
+            } else if (direction === 'left') {
+                path.push({ x: nc.x1 - 10, y: nc.y1 });
+            }
+            direction = 'horizontal';
             anchors.forEach((anchor) => {
-                path.push({ x: anchor.x, y: anchor.y });
+                if (direction === 'vertical') {
+                    path.push({
+                        x: path[path.length - 1].x + (anchor.x - path[path.length - 1].x) / 2,
+                        y: path[path.length - 1].y,
+                    });
+                    path.push({
+                        x: path[path.length - 2].x + (anchor.x - path[path.length - 2].x) / 2,
+                        y: anchor.y,
+                    });
+                    path.push({
+                        x: anchor.x,
+                        y: anchor.y,
+                    });
+                    direction = 'horizontal';
+                } else {
+                    path.push({
+                        x: path[path.length - 1].x,
+                        y: path[path.length - 1].y + (anchor.y - path[path.length - 1].y) / 2,
+                    });
+                    path.push({
+                        x: anchor.x,
+                        y: path[path.length - 2].y + (anchor.y - path[path.length - 2].y) / 2,
+                    });
+                    path.push({
+                        x: anchor.x,
+                        y: anchor.y,
+                    });
+                    direction = 'vertical';
+                }
             });
-            path.push({ x: nc.x2, y: nc.y2 });
+            if (direction === 'vertical') {
+                path.push({
+                    x: path[path.length - 1].x + (nc.x2 - path[path.length - 1].x) / 2,
+                    y: path[path.length - 1].y,
+                });
+                path.push({
+                    x: path[path.length - 2].x + (nc.x2 - path[path.length - 2].x) / 2,
+                    y: nc.y2,
+                });
+                path.push({
+                    x: nc.x2,
+                    y: nc.y2,
+                });
+            } else {
+                path.push({
+                    x: path[path.length - 1].x,
+                    y: path[path.length - 1].y + (nc.y2 - path[path.length - 1].y) / 2,
+                });
+                path.push({
+                    x: nc.x2 - 10,
+                    y: path[path.length - 2].y + (nc.y2 - path[path.length - 2].y) / 2,
+                });
+                path.push({
+                    x: nc.x2 - 10,
+                    y: nc.y2,
+                });
+                path.push({
+                    x: nc.x2,
+                    y: nc.y2,
+                });
+            }
             return path;
         }
 
