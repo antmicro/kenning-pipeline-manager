9c4f4f6fa01c6d2508532945288d1be8b747af78
Mateusz Leonowicz <mleonowicz@antmicro.com>
2023-09-05 11:27:18 +0200

    [#48626] frontend: components: BlurPanel: Implement BlurPanel
    Signed-off-by: Mateusz Leonowicz <mleonowicz@antmicro.com>

---
+++ pipeline_manager/frontend/src/components/BlurPanel.vue
@@ -0,0 +1,45 @@
+<!--
+Copyright (c) 2022-2023 Antmicro <www.antmicro.com>
+
+SPDX-License-Identifier: Apache-2.0
+-->
+
+<template>
+    <div class="help-panel">
+        <slot />
+    </div>
+</template>
+
+<style lang="scss">
+.help-panel {
+    background-color: #{$gray-600}80;
+    backdrop-filter: blur(10px);
+    position: absolute;
+    padding: $spacing-l;
+    color: white;
+    left: 0%;
+    z-index: 10;
+    top: 0%;
+    width: 100%;
+    height: 100%;
+    min-height: fit-content;
+
+    & > .panel {
+        display: grid;
+        grid-row-gap: $spacing-l;
+        user-select: none;
+        right: 25%;
+        top: 25%;
+        width: 50%;
+        height: 50%;
+
+        & > div {
+            & > .option-label {
+                padding-bottom: $spacing-s;
+                color: $white;
+                font-size: $fs-medium;
+            }
+        }
+    }
+}
+</style>
