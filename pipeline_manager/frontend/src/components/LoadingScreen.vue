04f2cd14093663be67c11ff006a06a38e04c2e62
Eryk Szpotanski <eszpotanski@antmicro.com>
2023-09-11 17:03:43 +0200

    [#48812] frontend: components: Implement LoadingScreen
    Signed-off-by: Eryk Szpotanski <eszpotanski@antmicro.com>

---
+++ pipeline_manager/frontend/src/components/LoadingScreen.vue
@@ -0,0 +1,49 @@
+<!--
+Copyright (c) 2022-2023 Antmicro <www.antmicro.com>
+
+SPDX-License-Identifier: Apache-2.0
+-->
+
+<!--
+Represents Loading Screen shown when JSON files
+with specification and graph are being downloaded and loaded.
+-->
+
+<template>
+  <div class="loading-screen">
+    <SelfBuildingSquareSpinner
+      id="loading-screen-spinner"
+      :size="80"
+    />
+  </div>
+</template>
+
+<script>
+import { SelfBuildingSquareSpinner } from 'epic-spinners';
+
+export default {
+    components: {
+        SelfBuildingSquareSpinner,
+    },
+};
+</script>
+
+<style lang="scss">
+.loading-screen {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  z-index: 99;
+  background-color: #{$gray-600}80;
+}
+
+#loading-screen-spinner {
+  & > .square {
+    background: $green !important;
+  }
+}
+</style>
