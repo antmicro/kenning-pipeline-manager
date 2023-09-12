a2c44ce864afbd1d57e094f2711215367b80b7cc
Eryk Szpotanski <eszpotanski@antmicro.com>
2023-09-12 12:17:12 +0200

    [#48539] frontend: custom: CustomNode: Move getOptionName to separate module
    Signed-off-by: Eryk Szpotanski <eszpotanski@antmicro.com>

---
+++ pipeline_manager/frontend/src/custom/CustomNode.js
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2022-2023 Antmicro <www.antmicro.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * Implements additional functions for CustomNode
+ */
+
+/**
+ * The function decides whether a name for the option should be displayed.
+ *
+ * @param optionType Name of the option component
+ * @returns True if the name should be displayed, false otherwise.
+ */
+export default function getOptionName(optionType) {
+    switch (optionType) {
+        case 'InputInterface':
+        case 'SelectInterface':
+        case 'ListInterface':
+        case 'TextInterface':
+        case 'HexInterface':
+            return true;
+        case 'NumberInterface':
+        case 'IntegerInterface':
+        case 'CheckboxInterface':
+        case 'SliderInterface':
+        case 'NodeInterface':
+        default:
+            return false;
+    }
+}
