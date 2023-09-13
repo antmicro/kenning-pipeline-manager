eb34e797e6230a570147d21608f74a324f423f40
Grzegorz Latosinski <glatosinski@antmicro.com>
2023-09-13 13:52:20 +0200

    [#49116] tests: test_dataflow_specification: Added tests for missing interfaces/properties corner cases
    Signed-off-by: Grzegorz Latosinski <glatosinski@antmicro.com>

--- pipeline_manager/tests/test_dataflow_specification.py
+++ pipeline_manager/tests/test_dataflow_specification.py
@@ -39,20 +39,6 @@ def specification_invalid_property_type():
 
 
 @pytest.fixture
-def specification_missing_interfaces():
-    return {
-        'nodes': [
-            {
-                'name': 'TestNode',
-                'layer': 'TestNode',
-                'category': 'TestNode',
-                'properties': []
-            }
-        ]
-    }
-
-
-@pytest.fixture
 def specification_invalid_property_value():
     return {
         'nodes': [
@@ -87,25 +73,85 @@ def specification_invalid_nodes_without_name():
     }
 
 
-def test_example_specification(sample_specification):
+@pytest.fixture
+def specification_valid_nodes_without_properties():
+    return {
+        'nodes': [
+            {
+                'name': 'TestNode',
+                'layer': 'TestNode',
+                'category': 'TestNode',
+                'interfaces': []
+            }
+        ]
+    }
+
+
+@pytest.fixture
+def specification_valid_nodes_without_interfaces():
+    return {
+        'nodes': [
+            {
+                'name': 'TestNode',
+                'layer': 'TestNode',
+                'category': 'TestNode',
+                'properties': []
+            }
+        ]
+    }
+
+
+@pytest.fixture
+def specification_valid_nodes_without_layer():
+    return {
+        'nodes': [
+            {
+                'name': 'TestNode',
+                'category': 'TestNode',
+                'properties': [],
+                'interfaces': []
+            }
+        ]
+    }
+
+
+def test_example_specification(
+        prepare_validation_environment,
+        sample_specification):
     assert check_validation(sample_specification) == 0
+
+
+@pytest.mark.parametrize(
+    'valid_specification', [
+        'specification_valid_nodes_without_properties',
+        'specification_valid_nodes_without_interfaces',
+        'specification_valid_nodes_without_layer',
+    ]
+)
+def test_valid_specification(
+        prepare_validation_environment,
+        valid_specification,
+        request):
+    valid_specification = request.getfixturevalue(valid_specification)
+    assert check_validation(valid_specification) == 0
 
 
 @pytest.mark.parametrize(
     'invalid_specification', [
         'specification_invalid_property_type',
-        'specification_missing_interfaces',
         'specification_invalid_property_value',
         'specification_invalid_nodes_without_name',
     ]
 )
 def test_invalid_specification(
+        prepare_validation_environment,
         invalid_specification,
         request):
     invalid_specification = request.getfixturevalue(invalid_specification)
     assert check_validation(invalid_specification) == 1
 
 
-def test_example_specification_without_nodes(sample_specification):  # noqa: E501
+def test_example_specification_without_nodes(
+        sample_specification):
     del sample_specification['nodes']
     assert check_validation(sample_specification) == 1
