5a04ab96bfa8a58ddb2112ed1ecc116ed506613a
Eryk Szpotanski <eszpotanski@antmicro.com>
2023-10-24 18:18:44 +0200

    [#50609] backend: tcp_socket: Implement socket method working on separate thread
    Signed-off-by: Eryk Szpotanski <eszpotanski@antmicro.com>

---
+++ pipeline_manager/backend/tcp_socket.py
@@ -0,0 +1,72 @@
+# Copyright (c) 2022-2023 Antmicro <www.antmicro.com>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import json
+import threading
+from typing import Dict
+from flask_socketio import SocketIO
+
+from pipeline_manager.backend.state_manager import global_state_manager
+from pipeline_manager_backend_communication.misc_structures import Status
+from pipeline_manager_backend_communication.communication_backend import CommunicationBackend  # noqa: E501
+
+_THREAD: threading.Thread = None
+
+
+def manage_socket_messages(
+    tcp_server: CommunicationBackend,
+    socketio: SocketIO
+):
+    """
+    Function receiving messages from socket and redirecting it to WebSocket.
+
+    It runs as long as socket is connected.
+
+    Parameters
+    ----------
+    tcp_server : CommunicationBackend
+        Server managing socket
+    socketio : SocketIo
+        WebSocket connected to frontend
+    """
+    while True:
+        message = tcp_server.wait_for_message()
+        if message.status == Status.DATA_READY:
+            if isinstance(message.data[1], Dict):
+                data = message.data[1]
+            else:
+                data = json.loads(
+                    message.data[1].encode(tcp_server.encoding_format)
+                )
+            if message.data[0] is None:
+                # Message has no method -- it is response
+                event = 'api-response'
+            else:
+                # Message has methods -- it is request
+                event = 'api'
+            socketio.emit(event, data)
+        elif message.status == Status.CONNECTION_CLOSED:
+            break
+
+
+def start_socket_thread(socketio: SocketIO):
+    """
+    Starts thread with function redirecting messages from external app
+    to frontend.
+
+    If previously satarted thread is still alive, exception will be raised.
+
+    Parameters
+    ----------
+    socketio : SocketIo
+        WebSocket connected to frontend
+    """
+    global _THREAD
+    if _THREAD and _THREAD.is_alive():
+        raise Exception("Previous thread is still alive")
+    _THREAD = threading.Thread(
+        target=manage_socket_messages,
+        args=(global_state_manager.tcp_server, socketio)
+    )
+    _THREAD.start()
