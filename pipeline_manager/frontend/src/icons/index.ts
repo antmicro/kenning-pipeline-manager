271b06677f7824d43dad855b749f6ef8227400b2
Mateusz Leonowicz <mleonowicz@antmicro.com>
2023-11-08 12:49:08 +0100

    [#51152] frontend: icons: index: Create barrel file for icons
    Signed-off-by: Mateusz Leonowicz <mleonowicz@antmicro.com>

---
+++ pipeline_manager/frontend/src/icons/index.ts
@@ -0,0 +1,35 @@
+import Arrow from './Arrow.vue';
+import Backend from './Backend.vue';
+import Bell from './Bell.vue';
+import Bin from './Bin.vue';
+import Cogwheel from './Cogwheel.vue';
+import Cross from './Cross.vue';
+import Cube from './Cube.vue';
+import Disconnect from './Disconnect.vue';
+import ExclamationMark from './ExclamationMark.vue';
+import Logo from './Logo.vue';
+import Magnifier from './Magnifier.vue';
+import Pencil from './Pencil.vue';
+import Run from './Run.vue';
+import Sidebar from './Sidebar.vue';
+import StopDataflow from './StopDataflow.vue';
+import Validate from './Validate.vue';
+
+export default {
+    Arrow,
+    Backend,
+    Bell,
+    Bin,
+    Cogwheel,
+    Cross,
+    Cube,
+    Disconnect,
+    ExclamationMark,
+    Logo,
+    Magnifier,
+    Pencil,
+    Run,
+    Sidebar,
+    StopDataflow,
+    Validate,
+};
