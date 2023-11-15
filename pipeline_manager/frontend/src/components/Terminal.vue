50ffa93443eaa63a319bd39715083af9644eccc5
Grzegorz Latosinski <glatosinski@antmicro.com>
2023-11-15 18:43:27 +0100

    [#50292] frontend: components: Terminal: Initial embedding of hterm terminal
    Signed-off-by: Grzegorz Latosinski <glatosinski@antmicro.com>

--- pipeline_manager/frontend/src/components/Terminal.vue
+++ pipeline_manager/frontend/src/components/Terminal.vue
@@ -5,33 +5,110 @@ SPDX-License-Identifier: Apache-2.0
 -->
 
 <!--
-List of terminal messages that creates a `TerminalCommand` component for each message registered.
+Container for the hterm.js terminal.
 -->
 
 <template>
     <div class="terminal-container">
-        <div class="command-results" ref="commands">
-            <ul>
-                <li v-for="(log, index) in terminalStore.logs" v-bind:key="index">
-                    <TerminalCommand :message="log" />
-                </li>
-            </ul>
-        </div>
+        <div id="hterm-terminal"></div>
     </div>
 </template>
 
 <script>
-import TerminalCommand from './TerminalCommand.vue';
 import { terminalStore } from '../core/stores';
+import { hterm, lib } from '../third-party/hterm_all';
+
+const htermSettings = {
+    'background-color': '#1d1d1d',
+    'cursor-color': 'white',
+};
+
+const setHTermPreferences = () => {
+    Object.keys(htermSettings).forEach((key) => {
+        localStorage.setItem(
+            `/hterm/profiles/default/${key}`,
+            htermSettings[key],
+        );
+    });
+};
 
 export default {
-    components: {
-        TerminalCommand,
-    },
     data() {
         return {
+            term: undefined,
             terminalStore,
         };
+    },
+    async mounted() {
+        // wait for hterm.js library to load
+        // (glatosinski: we may later need to move it to some global scope)
+        await lib.init();
+        // configure hterm.js style and instantiate the terminal
+        setHTermPreferences();
+        this.term = new hterm.Terminal();
+
+        this.term.onTerminalReady = function onTerminalReady() {
+            // load logs that have existed already in the storage.
+            terminalStore.logs.forEach((log, index) => {
+                if (index > 0) this.io.println('\r\n\r\n');
+                this.io.println(log.replace(/\n/g, '\r\n'));
+            });
+            // for now configure the terminal as read-only
+            this.onVTKeystroke = (_string) => {};
+            this.io.sendString = this.io.print;
+            this.setCursorVisible(false);
+            this.installKeyboard();
+        };
+        // pin hterm.js in the template
+        this.term.decorate(document.querySelector('#hterm-terminal'));
+    },
+    computed: {
+        /**
+        * The direct access to store logs.
+        */
+        logs() {
+            return this.terminalStore.logs;
+        },
+    },
+    methods: {
+        /**
+        * Prints the current log line.
+        * This method automatically adds cariage return.
+        *
+        * @param log log entry string to display
+        */
+        printLog(log) {
+            if (this.term === undefined) return;
+            this.term.io.println(log.replace(/\n/g, '\r\n'));
+        },
+        /**
+        * Clears the display.
+        */
+        clearLog() {
+            if (this.term === undefined) return;
+            this.term.clearHome();
+        },
+    },
+    watch: {
+        logs: {
+            handler(val, oldval) {
+                if (val === undefined) {
+                    this.clearlog();
+                }
+                if (val.length === 0) {
+                    this.clearLog();
+                }
+                val.forEach((log, index) => {
+                    if (oldval !== undefined && log === oldval[index]) {
+                        return;
+                    }
+                    if (index > 0) this.printLog('\n\n');
+                    this.printLog(log);
+                });
+            },
+            immediate: true,
+            deep: true,
+        },
     },
 };
 </script>
@@ -53,19 +130,5 @@ export default {
             margin-right: $spacing-m;
         }
     }
-}
-
-input {
-    font-family: $roboto-mono;
-    bottom: 0;
-    width: calc(100% - 40px);
-    max-width: 100%;
-    height: 55px;
-    background-color: inherit;
-    border: none;
-    border-top: 1px solid $gray-500;
-    color: $white;
-    padding: 0 $spacing-l;
-    outline: none;
 }
 </style>
