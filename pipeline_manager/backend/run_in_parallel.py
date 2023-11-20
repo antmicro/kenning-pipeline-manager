c77492a7f790b4e17b6c01650c2ca920adfd6d48
Eryk Szpotanski <eszpotanski@antmicro.com>
2023-11-20 15:17:05 +0100

    [#51350] backend: run_in_parallel: Allow to run more than one process
    Signed-off-by: Eryk Szpotanski <eszpotanski@antmicro.com>

--- pipeline_manager/backend/run_in_parallel.py
+++ pipeline_manager/backend/run_in_parallel.py
@@ -9,17 +9,17 @@ Allows to run Pipeline Manager in parallel to the script
 communicating with it.
 """
 
+import asyncio
 import logging
 from multiprocessing import Process
 from pathlib import Path
-
-from pipeline_manager_backend_communication.misc_structures import Status
 
 from pipeline_manager.backend.state_manager import global_state_manager
 
 
 def server_process_handler(
-    frontend_path: Path, backend_host: str, backend_port: int
+    frontend_path: Path, backend_host: str, backend_port: int,
+    tcp_host: str, tcp_port: int,
 ):
     """
     Function ran as a process target, responsible for initializing the tcp
@@ -33,31 +33,19 @@ def server_process_handler(
         Port of the backend of Pipeline Manager
     """
 
-    tcp_server = global_state_manager.tcp_server
-    tcp_server.initialize_server()
-
-    logging.log(logging.INFO, "Connect the application to run start.")
-    out = tcp_server.wait_for_client()
-
-    if out.status != Status.CLIENT_CONNECTED:
-        logging.log(logging.WARNING, "External application did not connect")
-
     from pipeline_manager.backend.fastapi import create_app
     from pipeline_manager.backend.socketio import create_socketio
-    from pipeline_manager.backend.tcp_socket import start_socket_thread
     from pipeline_manager.backend.run_backend import run_uvicorn
-
     app = create_app(frontend_path)
     sio = create_socketio()
 
     app.static_folder = Path(frontend_path).resolve()
     app.template_folder = Path(frontend_path).resolve()
 
-    start_socket_thread(sio)
-    run_uvicorn(app, sio, backend_host, backend_port)
+    run_uvicorn(app, sio, backend_host, backend_port, tcp_host, tcp_port)
 
 
-def start_server_in_parallel(
+async def start_server_in_parallel(
     frontend_path: Path,
     tcp_server_host: str = "127.0.0.1",
     tcp_server_port: int = 9000,
@@ -90,21 +78,27 @@ def start_server_in_parallel(
     """
 
     logging.basicConfig(level=verbosity)
-    global_state_manager.reinitialize(tcp_server_port, tcp_server_host)
+    await global_state_manager.reinitialize(tcp_server_port, tcp_server_host)
 
-    global_state_manager.server_process = Process(
+    global_state_manager.server_processes.append(Process(
         target=server_process_handler,
-        args=(frontend_path, backend_host, backend_port),
-    )
-    global_state_manager.server_process.start()
+        args=(frontend_path, backend_host, backend_port,
+              tcp_server_host, tcp_server_port),
+    ))
+    global_state_manager.server_processes[-1].start()
 
 
-def stop_parallel_server():
+def stop_parallel_server(process_index: int):
     """
     Gracefully terminates the parallel server process.
+
+    Parameters
+    ----------
+    process_index : int
+        Index of the process that should be terminated
     """
-    global_state_manager.server_process.terminate()
+    global_state_manager.server_processes[process_index].terminate()
 
 
 if __name__ == "__main__":
-    start_server_in_parallel()
+    asyncio.run(start_server_in_parallel())
