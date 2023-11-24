5bf160b853536094242c252508d63e1f8b0beed2
Mateusz Leonowicz <mleonowicz@antmicro.com>
2023-11-24 16:29:53 +0100

    [#50292] frontend: components: Terminal: Switch to Components API
    Signed-off-by: Mateusz Leonowicz <mleonowicz@antmicro.com>

--- pipeline_manager/frontend/src/components/Terminal.vue
+++ pipeline_manager/frontend/src/components/Terminal.vue
@@ -15,105 +15,88 @@ Container for the hterm.js terminal.
 </template>
 
 <script>
+import {
+    defineComponent, computed, onMounted, watch,
+} from 'vue';
 import { terminalStore } from '../core/stores';
 import { hterm, lib } from '../third-party/hterm_all';
 
-const htermSettings = {
-    'background-color': '#1d1d1d',
-    'cursor-color': 'white',
-    'mouse-right-click-paste': false,
-    'pass-meta-v': false,
-    'mouse-paste-button': 'no-button',
-};
-
-const setHTermPreferences = () => {
-    Object.keys(htermSettings).forEach((key) => {
-        localStorage.setItem(
-            `/hterm/profiles/default/${key}`,
-            htermSettings[key],
-        );
-    });
-};
-
-export default {
-    data() {
-        return {
-            term: undefined,
-            terminalStore,
+export default defineComponent({
+    setup() {
+        let term;
+        const htermSettings = {
+            'background-color': '#1d1d1d',
+            'cursor-color': 'white',
+            'mouse-right-click-paste': false,
+            'pass-meta-v': false,
+            'mouse-paste-button': 'no-button',
         };
-    },
-    async mounted() {
-        // wait for hterm.js library to load
-        // (glatosinski: we may later need to move it to some global scope)
-        await lib.init();
-        // configure hterm.js style and instantiate the terminal
-        setHTermPreferences();
-        this.term = new hterm.Terminal();
 
-        this.term.onTerminalReady = function onTerminalReady() {
-            // load logs that have existed already in the storage.
-            terminalStore.logs.forEach((log, index) => {
-                if (index > 0) this.io.println('\r\n\r\n');
-                this.io.println(log.replace(/\n/g, '\r\n'));
+        const setHTermPreferences = () => {
+            Object.keys(htermSettings).forEach((key) => {
+                localStorage.setItem(
+                    `/hterm/profiles/default/${key}`,
+                    htermSettings[key],
+                );
             });
-            // for now configure the terminal as read-only
-            this.onVTKeystroke = (_string) => {};
-            this.io.sendString = (_string) => {};
-            this.setCursorVisible(false);
-            this.installKeyboard();
         };
-        // pin hterm.js in the template
-        this.term.decorate(document.querySelector('#hterm-terminal'));
-    },
-    computed: {
-        /**
-        * The direct access to store logs.
-        */
-        logs() {
-            return this.terminalStore.logs;
-        },
-    },
-    methods: {
-        /**
-        * Prints the current log line.
-        * This method automatically adds cariage return.
-        *
-        * @param log log entry string to display
-        */
-        printLog(log) {
-            if (this.term === undefined) return;
-            this.term.io.println(log.replace(/\n/g, '\r\n'));
-        },
-        /**
-        * Clears the display.
-        */
-        clearLog() {
-            if (this.term === undefined) return;
-            this.term.clearHome();
-        },
-    },
-    watch: {
-        logs: {
-            handler(val, oldval) {
-                if (val === undefined) {
-                    this.clearlog();
-                }
-                if (val.length === 0) {
-                    this.clearLog();
-                }
-                val.forEach((log, index) => {
-                    if (oldval !== undefined && log === oldval[index]) {
-                        return;
-                    }
-                    if (index > 0) this.printLog('\n\n');
-                    this.printLog(log);
+
+        onMounted(async () => {
+            // wait for hterm.js library to load
+            // (glatosinski: we may later need to move it to some global scope)
+            await lib.init();
+            // configure hterm.js style and instantiate the terminal
+            setHTermPreferences();
+            term = new hterm.Terminal();
+
+            term.onTerminalReady = function onTerminalReady() {
+                // load logs that have existed already in the storage.
+                terminalStore.logs.forEach((log, index) => {
+                    if (index > 0) this.io.println('\r\n\r\n');
+                    this.io.println(log.replace(/\n/g, '\r\n'));
                 });
-            },
+                // for now configure the terminal as read-only
+                this.onVTKeystroke = (_string) => {};
+                this.io.sendString = (_string) => {};
+                this.setCursorVisible(false);
+                this.installKeyboard();
+            };
+            // pin hterm.js in the template
+            term.decorate(document.querySelector('#hterm-terminal'));
+        });
+
+        const logs = computed(() => terminalStore.logs);
+
+        const printLog = (log) => {
+            if (term === undefined) return;
+            term.io.println(log.replace(/\n/g, '\r\n'));
+        };
+
+        const clearLog = () => {
+            if (term === undefined) return;
+            term.clearHome();
+        };
+
+        watch(logs, (val, oldval) => {
+            if (val === undefined) {
+                clearLog();
+            }
+            if (val.length === 0) {
+                clearLog();
+            }
+            val.forEach((log, index) => {
+                if (oldval !== undefined && log === oldval[index]) {
+                    return;
+                }
+                if (index > 0) printLog('\n\n');
+                printLog(log);
+            });
+        }, {
             immediate: true,
             deep: true,
-        },
+        });
     },
-};
+});
 </script>
 
 <style lang="scss" scoped>
