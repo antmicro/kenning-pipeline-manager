b6ab4c82f46ac3845d3f3abdb0136569ad0cf7b8
Eryk Szpotanski <eszpotanski@antmicro.com>
2023-11-30 15:27:27 +0100

    [#51981] frontend: communication: runInformation: Move runInfo to separate file and implement Map with default values
    Signed-off-by: Eryk Szpotanski <eszpotanski@antmicro.com>

---
+++ pipeline_manager/frontend/src/core/communication/runInformation.ts
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2022-2023 Antmicro <www.antmicro.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/* eslint-disable max-classes-per-file */
+
+/**
+ * Class with information about ran procedure.
+ */
+class RunInfo {
+    procedureName;
+
+    /** @private */
+    private pr_inProgress = false;
+
+    /**
+     * @param procedureName Name of the procedure
+     */
+    constructor(procedureName: string) {
+        this.procedureName = procedureName;
+    }
+
+    /**
+     * Finds progress bar affiliated with run.
+     * If cannot be found, throws Error.
+     */
+    get progressBar() {
+        const progressBar = document.querySelector<HTMLDivElement>(
+            `#navbar-button-${this.procedureName} > .progress-bar`,
+        );
+        if (!progressBar) throw new Error(`Progress bar does not exist for method: ${this.procedureName}`);
+        return progressBar;
+    }
+
+    /**
+     * Specifies if run is in progress.
+     */
+    get inProgress() {
+        return this.pr_inProgress;
+    }
+
+    set inProgress(value) {
+        const { progressBar } = this;
+        if (!value) progressBar.classList.remove('animate');
+        progressBar.style.width = '0%';
+        this.pr_inProgress = value;
+    }
+}
+
+/**
+ * @template K key type
+ * @template V value type
+ * @extends {Map<K,V>}
+ *
+ * Map automatically creating values if key is not found.
+ */
+class DefaultMap<K, V> extends Map<K, V> {
+    defaultFactory;
+
+    constructor(defaultFactory: (key: K) => V) {
+        super();
+        this.defaultFactory = defaultFactory;
+    }
+
+    get(key: K): V {
+        if (!super.has(key)) {
+            super.set(key, this.defaultFactory(key));
+        }
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        return super.get(key)!;
+    }
+}
+
+export default new DefaultMap<string, RunInfo>((key) => new RunInfo(key));
