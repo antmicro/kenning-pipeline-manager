55a9a8bd1341d5cb9a800f38efd4e8bfe68c4450
Grzegorz Latosinski <glatosinski@antmicro.com>
2023-12-14 10:34:21 +0100

    [#52381] GH Actions: lint-commits: Added possibility to pass refs to check
    Signed-off-by: Grzegorz Latosinski <glatosinski@antmicro.com>

--- .github/scripts/lint-commits.sh
+++ .github/scripts/lint-commits.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
+# Copyright (c) 2022-2023 Antmicro <www.antmicro.com>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+# Runs linting on commits between FROM_REF and TO_REF
+
+FROM_REF=${1:-$PRE_COMMIT_FROM_REF}
+TO_REF=${2:-$PRE_COMMIT_TO_REF}
+
+if [[ "$FROM_REF" == "" || "$TO_REF" == "" ]]; then
+    echo "Script accepts two arguments - FROM_REF and TO_REF"
+    exit 1
+fi
+
 echo -e "Commits to push:\n"
 
-git log --format=format:-------%n%B%n------- $PRE_COMMIT_FROM_REF..$PRE_COMMIT_TO_REF | cat -n
+git log --format=format:-------%n%B%n------- $FROM_REF..$TO_REF | cat -n
 
 echo -e "\n\nErrors:\n"
 
-git log --format=format:-------%n%B%n------- $PRE_COMMIT_FROM_REF..$PRE_COMMIT_TO_REF | typos -
+git log --format=format:-------%n%B%n------- $FROM_REF..$TO_REF | typos - && echo "No errors"
