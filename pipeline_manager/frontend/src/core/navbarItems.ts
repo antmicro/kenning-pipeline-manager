68100c318b326072176f7ade3cfc1254d7fc1491
Mateusz Leonowicz <mleonowicz@antmicro.com>
2024-04-23 15:41:33 +0200

    [#57712] frontend: Update exemplary navbarItems
    Signed-off-by: Mateusz Leonowicz <mleonowicz@antmicro.com>

--- pipeline_manager/frontend/src/core/navbarItems.ts
+++ pipeline_manager/frontend/src/core/navbarItems.ts
@@ -1,5 +1,6 @@
 export interface NavbarItem {
     name: string,
+    stopName?: string,
     iconName: string,
     icon?: any, // eslint-disable-line @typescript-eslint/no-explicit-any
     procedureName: string
