9efbc565f8c6fa6a757bf16c18a947eb56869a61
Mateusz Leonowicz <mleonowicz@antmicro.com>
2024-07-08 15:36:12 +0200

    [#62121] tests: test_dataflow: Add tests for incompatible interfaces connected
    Signed-off-by: Mateusz Leonowicz <mleonowicz@antmicro.com>

--- pipeline_manager/tests/test_dataflow.py
+++ pipeline_manager/tests/test_dataflow.py
@@ -55,6 +55,16 @@ from pipeline_manager.tests.conftest import check_validation
             "dataflow_invalid_node_property_select_value_bool",
             2,
         ),
+        (
+            "dataflow_specification_two_incompatible_nodes",
+            "dataflow_graph_incompatible_interfaces_connected",
+            2,
+        ),
+        (
+            "dataflow_specification_incompatible_node_and_graph_node",
+            "dataflow_graph_incompatible_interfaces_connected_graph_node",
+            2,
+        ),
     ],
 )
 def test_dataflow_expected(
@@ -111,6 +121,63 @@ def dataflow_specification_node_property_select(
         }
     ]
     return dataflow_specification_node_no_properties
+
+
+# ----------------------------------
+# Specifications with nodes that have incompatible interfaces
+# ----------------------------------
+
+
+@pytest.fixture
+def dataflow_specification_two_incompatible_nodes(
+    dataflow_specification_node_no_properties
+):
+    dataflow_specification_node_no_properties["nodes"].append(
+        {
+            "name": "EncodeVideo",
+            "category": "Filesystem",
+            "interfaces": [
+                {"name": "encoding", "type": ["Text"], "direction": "inout"}
+            ],
+        }
+    )
+    return dataflow_specification_node_no_properties
+
+
+@pytest.fixture
+def dataflow_specification_incompatible_node_and_graph_node(
+    dataflow_specification_two_incompatible_nodes
+):
+    dataflow_specification_two_incompatible_nodes["graphs"] = []
+    dataflow_specification_two_incompatible_nodes["graphs"].append(
+        {
+            "name": "Subgraph #1",
+            "category": "Category #1",
+            "nodes": [
+                {
+                    "name": "EncodeVideo",
+                    "interfaces": [
+                        {
+                            "id": "40adf4d9-bf08-40ee-82f3-c95b4588dc32",
+                            "name": "encoding",
+                            "externalName": "External Encoding",
+                            "direction": "inout",
+                        }
+                    ],
+                    "position": {"x": 600, "y": 500},
+                    "twoColumn": False,
+                    "properties": [],
+                }
+            ],
+            "connections": [],
+        },
+    )
+    return dataflow_specification_two_incompatible_nodes
+
+
+# ----------------------------------
+# Dataflows with different properties
+# ----------------------------------
 
 
 @pytest.fixture
@@ -206,3 +273,91 @@ def dataflow_invalid_node_property_select_value_bool(
         0
     ]["value"] = False
     return dataflow_valid_node_property_select
+
+
+# ----------------------------------
+# Dataflows with incompatible interfaces connected
+# ----------------------------------
+
+
+@pytest.fixture
+def dataflow_graph_incompatible_interfaces_connected(
+    dataflow_node_base,
+):
+    dataflow_node_base["graphs"][0]["nodes"].append(
+        {
+            "name": "EncodeVideo",
+            "id": "a35f5cab-b374-45f0-abe2-15549714e48f",
+            "position": {"x": 2100, "y": 200},
+            "interfaces": [
+                {
+                    "direction": "inout",
+                    "id": "8de940b5-6076-485c-844d-93227281b5e3",
+                    "name": "encoding",
+                }
+            ],
+            "properties": [],
+            "twoColumn": True,
+        }
+    )
+    dataflow_node_base["graphs"][0]["connections"].append(
+        {
+            "id": "4b1a3e86-7c0b-411c-a580-b0a4c9fd2582",
+            "from": "8de940b5-6076-485c-844d-93227281b5e3",
+            "to": "6efb374c-a115-404e-ade8-0aa05ba93996",
+        }
+    )
+    return dataflow_node_base
+
+
+@pytest.fixture
+def dataflow_graph_incompatible_interfaces_connected_graph_node(
+    dataflow_node_base,
+):
+    dataflow_node_base["graphs"].append(
+        {
+            "id": "7060fa38-a0fe-4e44-8e5a-de5a2187680f",
+            "nodes": [
+                {
+                    "name": "EncodeVideo",
+                    "id": "231d20d8-811b-46e8-ba9a-6342032f6c85",
+                    "interfaces": [
+                        {
+                            "name": "encoding",
+                            "externalName": "External Encoding",
+                            "id": "6ba91cca-80d5-4598-a6ab-750ddb6cec83",
+                            "direction": "inout",
+                        }
+                    ],
+                    "position": {"x": 600, "y": 500},
+                    "twoColumn": False,
+                    "properties": [],
+                }
+            ],
+            "connections": [],
+        }
+    )
+    dataflow_node_base["graphs"][0]["nodes"].append(
+        {
+            "name": "Subgraph #1",
+            "id": "afe19cad-a7fd-465e-a05a-0b46ece51941",
+            "interfaces": [
+                {
+                    "name": "External Encoding",
+                    "id": "6ba91cca-80d5-4598-a6ab-750ddb6cec83",
+                    "direction": "inout",
+                }
+            ],
+            "position": {"x": 500, "y": 200},
+            "subgraph": "7060fa38-a0fe-4e44-8e5a-de5a2187680f",
+            "twoColumn": False,
+        }
+    )
+    dataflow_node_base["graphs"][0]["connections"].append(
+        {
+            "id": "4b1a3e86-7c0b-411c-a580-b0a4c9fd2582",
+            "from": "6ba91cca-80d5-4598-a6ab-750ddb6cec83",
+            "to": "6efb374c-a115-404e-ade8-0aa05ba93996",
+        }
+    )
+    return dataflow_node_base
