e07c4f188b98cbd105113dc29273422d9adb59c6
Mateusz Leonowicz <mleonowicz@antmicro.com>
2024-07-24 12:45:49 +0200

    [#62676] frontend: icons: Cross: Add rotation prop

--- pipeline_manager/frontend/src/icons/Cross.vue
+++ pipeline_manager/frontend/src/icons/Cross.vue
@@ -6,7 +6,7 @@ SPDX-License-Identifier: Apache-2.0
 
 <!-- eslint-disable max-len -->
 <template>
-    <svg width="9" height="9" viewBox="0 0 9 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg :style="rotationStyle" width="9" height="9" viewBox="0 0 9 9" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path :class="color" d="M1 1L8 8" stroke-width="1.5" />
         <path :class="color" d="M8 1L1 8" stroke-width="1.5" />
     </svg>
@@ -19,6 +19,18 @@ export default {
             type: String,
             required: false,
         },
+        rotate: {
+            type: Number,
+            default: 0,
+            required: false,
+        },
+    },
+    setup(props) {
+        const rotationStyle = {
+            rotate: `${props.rotate}deg`,
+        };
+
+        return { rotationStyle };
     },
 };
 </script>
