4a6e8d57719f56675b0cabe12d711daece2ee638
Mikołaj Szałkowski <mszalkowski@internships.antmicro.com>
2024-09-06 13:20:36 +0000

    [#72497] Added initial Sphinx extension for Pipeline Manager graphs

---
+++ pipeline_manager/sphinxext/__init__.py
@@ -0,0 +1,7 @@
+# Copyright (c) 2024-2025 Antmicro <www.antmicro.com>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+"""
+Sphinx extension for including Pipeline Manager's graphs in documentation.
+"""
