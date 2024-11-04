7b68760a998deb2a6a76c34932ac31ebcc8303e4
Igor Sosnowicz <isosnowicz@internships.antmicro.com>
2024-11-04 14:59:26 +0100

    [#62679] frontend: tests: Implemented simple Playwright test

--- pipeline_manager/frontend/tests/main.spec.ts
+++ pipeline_manager/frontend/tests/main.spec.ts
@@ -1,18 +1,8 @@
 import { test, expect } from '@playwright/test';
 
+const URL = "http://localhost:8080"
+
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await page.goto(URL);
+  expect(await page.title()).toBe('Pipeline Manager');
 });
