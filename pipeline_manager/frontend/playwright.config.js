d93763190033e5fb8f6b79ef3709303c06e81d9f
Igor Sosnowicz <isosnowicz@internships.antmicro.com>
2024-11-05 12:23:43 +0100

    [#62679] frontend: playwright.config: Configured tests to run in parallel by default

--- pipeline_manager/frontend/playwright.config.js
+++ pipeline_manager/frontend/playwright.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from '@playwright/test';
 
 
 export default defineConfig({
-
+  fullyParallel: true,
   globalSetup: 'tests/test-setup.js',
   testDir: 'tests',
   reporter: 'html',
