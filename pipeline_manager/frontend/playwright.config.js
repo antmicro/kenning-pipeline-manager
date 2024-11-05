3c871a48103e93d4c7c2b0dd6df3c5e760782875
Igor Sosnowicz <isosnowicz@internships.antmicro.com>
2024-11-05 10:47:45 +0100

    [#62679] frontend: Developed test of removing node, expanded config

--- pipeline_manager/frontend/playwright.config.js
+++ pipeline_manager/frontend/playwright.config.js
@@ -1,5 +1,11 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
-    globalSetup: 'tests/test-setup.js',
+  globalSetup: 'tests/test-setup.js',
+  testDir: 'tests',
+  reporter: 'html',
+
+  use: {
+    trace: 'on-first-retry',
+  },
 });
--- pipeline_manager/frontend/tests/main.spec.ts
+++ pipeline_manager/frontend/tests/main.spec.ts
@@ -1,8 +1,30 @@
 import { test, expect } from '@playwright/test';
 
-const URL = "http://localhost:8080"
+const BASE_URL = "http://localhost:8080";
+const SAMPLE_DATAFLOW = `${BASE_URL}/?spec=https%3A%2F%2Fraw.githubusercontent.com%2Fantmicro%2Fkenning-pipeline-manager%2Fmain%2Fexamples%2Fsample-specification.json&graph=https%3A%2F%2Fraw.githubusercontent.com%2Fantmicro%2Fkenning-pipeline-manager%2Fmain%2Fexamples%2Fsample-dataflow.json`;
+
 
 test('has title', async ({ page }) => {
-  await page.goto(URL);
+  await page.goto(SAMPLE_DATAFLOW) // Default address
   expect(await page.title()).toBe('Pipeline Manager');
+});
+
+
+test('remove node', async ({ page }) => {
+  await page.goto(SAMPLE_DATAFLOW)
+  const loadVideoNodeId = 'f50b4f2a-a2e2-4409-a5c9-891a8de44a5b'
+  await page.waitForSelector(`#${loadVideoNodeId}`);
+
+  const loadVideoNode = await page.locator(`#${loadVideoNodeId}`);
+  expect(loadVideoNode.isVisible());
+
+
+  loadVideoNode.click({
+    button: 'right'
+  });
+
+  const deleteButton = await page.getByText('Delete');
+  await deleteButton.click();
+
+  expect(await page.locator(`#${loadVideoNodeId}`).isVisible() === false);
 });
