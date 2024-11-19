c4e072eb53859a727f055fb7cc042bacf08633f9
Igor Sosnowicz <isosnowicz@internships.antmicro.com>
2024-11-19 10:02:47 +0100

    [#68555]: frontend: tests: subgraph: Added test checking if changes in subgraph are preserved after leaving it

--- pipeline_manager/frontend/tests/subgraph.spec.ts
+++ pipeline_manager/frontend/tests/subgraph.spec.ts
@@ -18,14 +18,13 @@ test('test entering subgraph', async ({ page }) => {
     await enterSubgraph(page, nodeContainingSubgraph);
 
     // A subgraph contains two nodes.
-    nodes = page.locator('.node-container > div');
     expect(await nodes.count()).toBe(2);
 });
 
 test('test coming back from subgraph', async ({ page }) => {
     await page.goto(getUrl('subgraph'));
 
-    // There are four nodes initially.
+    // Initially, there are four nodes.
     const nodes = page.locator('.node-container > div');
     expect(await nodes.count()).toBe(4);
 
@@ -34,8 +33,45 @@ test('test coming back from subgraph', async ({ page }) => {
     // A subgraph contains two nodes.
     expect(await nodes.count()).toBe(2);
 
-    // Leave a subgraph.
+    // Leave the subgraph.
     const leaveButton = page.getByText('Return from subgraph editor').locator('../..');
     await leaveButton.click();
     expect(await nodes.count()).toBe(4);
+});
+
+async function dragAndDrop(page: Page, locator: Locator, to: { x: number; y: number }) {
+    await locator.hover();
+    await page.mouse.down();
+    await page.mouse.move(to.x, to.y);
+    await page.mouse.up();
+}
+
+test('test preserving changes to subgraph', async ({ page }) => {
+    await page.goto(getUrl('subgraph'));
+
+    const nodes = page.locator('.node-container > div');
+    expect(await nodes.count()).toBe(4);
+
+    const nodeContainingSubgraph = page.getByText('Test subgraph node #1').nth(1).locator('../..');
+    await enterSubgraph(page, nodeContainingSubgraph);
+    expect(await nodes.count()).toBe(2);
+
+    // Add a new node: open the node browser, expand a category, and drag & drop a node.
+    const showNodesButton = page.getByText('Show node browser').locator('../..');
+    await showNodesButton.click();
+
+    const firstCategoryLabel = page.getByText('First Category');
+    await firstCategoryLabel.click();
+
+    const nodeFromBrowser = page.getByText('Test node #').first();
+    await dragAndDrop(page, nodeFromBrowser, { x: 400, y: 200 });
+    expect(await nodes.count()).toBe(3);
+
+    // Get back to the main graph.
+    const leaveButton = page.getByText('Return from subgraph editor').locator('../..');
+    await leaveButton.click();
+
+    // Enter the subgraph, it should have the same node count.
+    await enterSubgraph(page, nodeContainingSubgraph);
+    expect(await nodes.count()).toBe(3);
 });
