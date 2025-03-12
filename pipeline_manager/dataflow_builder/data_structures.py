b0069218ba6dee5842398d29fa8697c268ef3330
Igor Sosnowicz <isosnowicz@internships.antmicro.com>
2025-03-12 11:49:23 +0100

    [#71727] dataflow_builder: data_structures: Include license header

---
+++ pipeline_manager/dataflow_builder/data_structures.py
@@ -0,0 +1,21 @@
+# Copyright (c) 2025 Antmicro <www.antmicro.com>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+"""Module with data structure for entities of a DataflowGraph."""
+from enum import Enum
+
+
+class Direction(Enum):
+    """Available directions of an interface."""
+
+    INPUT = "input"
+    OUTPUT = "output"
+    INOUT = "inout"
+
+
+class Side(Enum):
+    """Sides, on which an interface may be located."""
+
+    LEFT = "left"
+    RIGHT = "right"
