051265682d3e092aaa764fc84f37114643349b2f
Igor Sosnowicz <isosnowicz@internships.antmicro.com>
2025-03-28 13:05:49 +0100

    [#70515] frontend_tester: rpc_client: Implement RPC client for test purposes

---
+++ pipeline_manager/frontend_tester/rpc_client.py
@@ -0,0 +1,55 @@
+"""Module with a RPC client for test purposes."""
+
+import asyncio
+import json
+from pathlib import Path
+
+from pipeline_manager_backend_communication.communication_backend import (
+    CommunicationBackend,
+)
+from pipeline_manager_backend_communication.misc_structures import MessageType
+
+host = "127.0.0.1"
+port = 9000
+
+
+class RPCMethods:
+    """Class implementing RPC methods by having methods matching names."""
+
+    def specification_get(self) -> dict:
+        path = Path("./examples/sample-subgraph-specification.json")
+        specification = {}
+        with open(path, "rt") as fd:
+            specification = json.load(fd)
+        return {"type": MessageType.OK.value, "content": specification}
+
+    # Method's parameters have to match with received message
+    # or **kwargs can be used to get all received params
+    def dataflow_validate(self, dataflow: dict) -> dict:
+        return {"type": MessageType.OK.value}
+
+    def dataflow_run(self, dataflow: dict) -> dict:
+        return {"type": MessageType.OK.value}
+
+    def dataflow_stop(self) -> dict:
+        return {"type": MessageType.OK.value}
+
+    def dataflow_export(self, dataflow: dict) -> dict:
+        return {"type": MessageType.OK.value, "content": dataflow}
+
+    def dataflow_import(self, **kwargs) -> dict:
+        return {
+            "type": MessageType.OK.value,
+            "content": kwargs["external_application_dataflow"],
+        }
+
+
+async def run_rpc_client():
+    """Run a RPC client with the default settings."""
+    client = CommunicationBackend(host, port)
+    await client.initialize_client(RPCMethods())
+    await client.start_json_rpc_client()
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(run_rpc_client())
