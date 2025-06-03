587045cfec057c6cc8f7f52e0fea91c3c57cf60b
Anna Roszkiewicz <aroszkiewicz@internships.antmicro.com>
2025-06-03 14:53:10 +0200

    [#56440] frontend: src: components: menu: nodeCreation: InterfaceListMenu.vue: Added component

---
+++ pipeline_manager/frontend/src/components/menu/nodeCreation/InterfaceListMenu.vue
@@ -0,0 +1,86 @@
+<!--
+Copyright (c) 2025 Antmicro <www.antmicro.com>
+
+SPDX-License-Identifier: Apache-2.0
+-->
+
+<!--
+Popup menu for configuring node interfaces.
+-->
+
+<!-- eslint-disable vue/no-mutating-props -->
+<template>
+    <div class="create-menu">
+        <div v-for="option in configurationOptions" :key="option.id">
+            <div class="option-label">
+                {{ getOptionName(option.componentName) ? `${option.name}:` : '' }}
+            </div>
+        </div>
+        <div v-for="(item, index) in items" :key="index">
+            <label>
+                <input type="checkbox" v-model="selectedItems" :value="item" />
+                {{ item.name }}
+            </label>
+        </div>
+        <component :is="removeInterface.component" :intf="removeInterface" />
+    </div>
+</template>
+
+<script lang="ts">
+import {
+    defineComponent, computed, ref,
+} from 'vue';
+import { ButtonInterface } from '@baklavajs/renderer-vue'; // eslint-disable-line object-curly-newline
+
+import { customNodeConfiguration } from '../../../core/nodeCreation/Configuration.ts';
+import { menuState } from '../../../core/nodeCreation/ConfigurationState.ts';
+
+export default defineComponent({
+    setup() {
+        const close = () => {
+            menuState.interfaceListMenu = false;
+        };
+
+        const items = customNodeConfiguration.interfaces;
+        const selectedItems = ref([]);
+
+        const removeInterface = computed(() => {
+            const button: any = new ButtonInterface('Remove interfaces', () => {
+                customNodeConfiguration.removeInterfaces(selectedItems.value);
+                close();
+            });
+            button.componentName = 'ButtonInterface';
+            return button;
+        });
+
+        return {
+            items,
+            selectedItems,
+            removeInterface,
+        };
+    },
+});
+</script>
+
+<style lang="scss">
+    .create-menu {
+        display: flex;
+        flex-direction: column;
+        gap: 1em;
+
+        height: max-content;
+
+        & > div {
+            & > .__name-option > .baklava-input {
+                // Padding is included into width
+                box-sizing: border-box;
+            }
+
+            & > .option-label {
+                padding-bottom: $spacing-s;
+                color: $white;
+                font-size: $fs-medium;
+            }
+        }
+    }
+</style>
