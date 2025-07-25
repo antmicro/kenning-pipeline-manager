9f5ebc5ba668df19361ab93851fb7ea7796be9a7
Andrii Chmutov <achmutov@internships.antmicro.com>
2025-07-25 12:25:01 +0200

    [#80401] frontend: communication: externalApp: frontend: Add implementation
    Signed-off-by: Andrii Chmutov <achmutov@internships.antmicro.com>

---
+++ pipeline_manager/frontend/src/core/communication/externalApp/frontend.ts
@@ -0,0 +1,18 @@
+import { JSONRPCRequest } from 'json-rpc-2.0';
+import ExternalApp, { EndpointType } from './base';
+
+export default class ExternalFrontendApp implements ExternalApp {
+    // eslint-disable-next-line no-useless-constructor
+    public constructor(public source: Window) {}
+
+    // eslint-disable-next-line class-methods-use-this
+    public async isConnected() { return true; }
+
+    // eslint-disable-next-line class-methods-use-this
+    public async onConnect() { return true; }
+
+    public request(data: JSONRPCRequest, endpoint: EndpointType) {
+        if (endpoint === 'backend-api') throw new Error('Frontend external app does not support backend endpoints');
+        this.source.postMessage(data);
+    }
+}
