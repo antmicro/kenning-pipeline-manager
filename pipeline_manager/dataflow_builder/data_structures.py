325c2398afbe357dd3e86db097265c2fcc5bdcd1
Igor Sosnowicz <isosnowicz@internships.antmicro.com>
2025-07-28 17:15:14 +0200

    [#81081] dataflow_builder: data_structures: Define custom exceptions

--- pipeline_manager/dataflow_builder/data_structures.py
+++ pipeline_manager/dataflow_builder/data_structures.py
@@ -5,6 +5,8 @@
 """Module with data structure for entities of a DataflowGraph."""
 from enum import Enum
 
+Infinity = None
+
 
 class Direction(Enum):
     """Available directions of an interface."""
@@ -21,4 +23,67 @@ class Side(Enum):
     RIGHT = "right"
 
 
-Infinity = None
+class InterfaceCountError(Exception):
+    """Raised if there is either too many or too few dynamic interfaces."""
+
+
+class MissingInterfaceAttributeError(Exception):
+    """Raised if an attribute was expected, but is missing."""
+
+
+class MultipleInterfacesSelectedError(Exception):
+    """
+    Raised if several interfaces were selected when only one was expected.
+    """
+
+
+class InvalidPropertyValueError(Exception):
+    """Raised if the value of a property is invalid."""
+
+
+class MissingPropertyError(Exception):
+    """Raised if a property with a given names is missing."""
+
+
+class OutOfSpecificationNodeError(Exception):
+    """Raised if a node is not present in the specification."""
+
+
+class SpecificationWithoutNodesError(Exception):
+    """Raised if a specification does not define any nodes."""
+
+
+class MissingNodeAttributeError(Exception):
+    """Raised if a given node lacks an expected attribute."""
+
+
+class InvalidPropertyTypeError(Exception):
+    """
+    Raised if an argument representing a property has an unrecognized type
+    (different than `Property` and `dict`).
+    """
+
+
+class InvalidInterfaceTypeError(Exception):
+    """
+    Raised if an argument representing an interface has an unrecognized type
+    (different than `Interface` and `dict`).
+    """
+
+
+class DuplicateExternalNamesError(Exception):
+    """Raised if interfaces share at least a single external name."""
+
+
+class NotDynamicInterfaceError(Exception):
+    """
+    Raised if a dynamic interface was expected
+    but non-dynamic one was provided.
+    """
+
+
+class NoInterfaceMatchedError(Exception):
+    """
+    Raised if no interface was matched
+    but at least or more than one had to be matched.
+    """
