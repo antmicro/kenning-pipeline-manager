16d48cc2acbbe65ade0e40f3a9cd274c66a5421c
Anna Roszkiewicz <aroszkiewicz@antmicro.com>
2025-08-05 19:41:40 +0200

    [#81156] frontend: tests: editabe_node_types.spec.ts: Added tests for editing extending nodes
    Signed-off-by: Anna Roszkiewicz <aroszkiewicz@antmicro.com>

--- pipeline_manager/frontend/tests/editable_node_types.spec.ts
+++ pipeline_manager/frontend/tests/editable_node_types.spec.ts
@@ -51,17 +51,49 @@ async function loadIncludeSpecification(page: Page) {
     await fileChooser.setFiles(getPathToJsonFile('sample-include-specification.json'));
 }
 
+async function assertInputCount(page: Page, nodeName: string, count: integer) {
+    const inputs = await page
+        .locator(`[data-node-type="${nodeName}"]`)
+        .locator('.__interfaces .__inputs > div')
+        .count();
+    expect(inputs).toBe(count);
+}
+
 async function addInterface(page: Page, nodeName: string) {
     const node = page.getByText(nodeName).last();
     await node.click({ button: 'right', force: true });
     await page.getByText('Add interface').click();
     await page.getByRole('button', { name: 'Add interface' }).click();
 
-    const inputs = await page
-        .locator('[data-node-type="Custom Node"]')
-        .locator('.__interfaces .__inputs > div')
-        .count();
-    expect(inputs).toBe(2);
+    await assertInputCount(page, nodeName, 2);
+}
+
+async function renameNodeType(page: Page, oldName: string, newName: string) {
+    const node = page.getByText(oldName).last();
+    await node.click({ button: 'right', force: true });
+    await page.getByText('Configure').click();
+    await page.getByRole('textbox').first().fill(newName);
+    await page.getByRole('button', { name: 'Configure' }).click();
+}
+
+async function addParentAndChildNode(page: Page, coord: integer) {
+    await addNode(page, 'Processing', 'Binary images', 750, 80);
+
+    await openNodePalette(page);
+    const nodePalette = page.locator('.baklava-node-palette');
+    const category = nodePalette.getByText('Processing', { exact: true });
+    await category.scrollIntoViewIfNeeded();
+    await expect(category).toBeVisible();
+
+    const categoryNodeEntry = nodePalette.getByText('Binary images');
+    const categoryNodeButton = categoryNodeEntry.locator('../..').locator('svg.arrow.right.small');
+    await categoryNodeButton.scrollIntoViewIfNeeded();
+    await expect(categoryNodeButton).toBeVisible();
+    await categoryNodeButton.click();
+
+    const childNodeEntry = nodePalette.getByText('Logical AND');
+    await expect(childNodeEntry).toBeVisible();
+    await dragAndDrop(page, childNodeEntry, 500, coord);
 }
 
 async function saveSpecificationAs(page: Page, filenameWithoutExtension: string): Promise<string> {
@@ -136,4 +168,78 @@ test('register custom node in specification with "include" keyword', async ({ pa
     await addNode(page, 'Default category', nodeName, 750, 80);
     const specificationPath = await saveSpecificationAs(page, 'new_specification');
     await verifyNodePresence(page, specificationPath, nodeName);
+});
+
+test('rename extending node', async ({ page }) => {
+    await page.goto(getUrl());
+    await loadIncludeSpecification(page);
+
+    await addNode(page, 'Filesystem', 'LoadVideo', 750, 80);
+    await enableEditingNodes(page);
+    await renameNodeType(page, 'LoadVideo', 'New node name');
+
+    // assert that both nodes are renamed
+    const editedNode = page.locator('[data-node-type="New node name"]');
+    expect(await editedNode.count()).toBe(2);
+
+    // assert that the node is renamed in node palette
+    const nodePalette = page.locator('.baklava-node-palette');
+    const category = nodePalette.getByText('Filesystem');
+
+    await enableNavigationBar(page);
+    await category.scrollIntoViewIfNeeded();
+    await expect(category).toBeVisible();
+    await category.click();
+
+    const newNodeEntry = nodePalette.getByText('New node name');
+    expect(newNodeEntry).toBeVisible();
+    const oldNodeEntry = nodePalette.getByText('LoadVideo', { exact: true });
+    expect(oldNodeEntry).not.toBeAttached();
+
+    // assert that both nodes have inherited properties
+    expect(editedNode.first().getByText('filename')).toBeVisible();
+    expect(editedNode.first().getByText('frames')).toBeVisible();
+    expect(editedNode.nth(1).getByText('filename')).toBeVisible();
+    expect(editedNode.nth(1).getByText('frames')).toBeVisible();
+
+    // assert that a new node has inherited properties
+    await dragAndDrop(page, newNodeEntry, 300, 300);
+    expect(editedNode.nth(2).getByText('filename')).toBeVisible();
+    expect(editedNode.nth(2).getByText('frames')).toBeVisible();
+});
+
+test('rename category node', async ({ page }) => {
+    await page.goto(getUrl());
+    await loadIncludeSpecification(page);
+
+    await addParentAndChildNode(page, 200);
+    await openNodePalette(page);
+
+    await renameNodeType(page, 'Binary images', 'New node name');
+    await assertInputCount(page, 'Logical AND', 2);
+
+    // check category in custom sidebar
+    const node = page.getByText('Logical AND').locator('..').last();
+    await node.click({ button: 'right'});
+    await page.getByText('Details').click();
+
+    const parents = page.getByText('Generalize');
+    const siblings = page.getByText('Choose other type');
+    await expect(parents).toBeVisible();
+    await expect(siblings).toBeVisible();
+});
+
+test('add interface to category node', async ({ page }) => {
+    await page.goto(getUrl());
+    await loadIncludeSpecification(page);
+
+    await addParentAndChildNode(page, 200);
+    await openNodePalette(page);
+
+    await addInterface(page, 'Binary images');
+    await assertInputCount(page, 'Logical AND', 3);
+
+    await addParentAndChildNode(page, 200);
+    await assertInputCount(page, 'Binary images', 4);
+    await assertInputCount(page, 'Logical AND', 6);
 });
