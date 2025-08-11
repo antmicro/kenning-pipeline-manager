38ea235e49cafcb0c390784e3a78643d40109802
Anna Roszkiewicz <aroszkiewicz@antmicro.com>
2025-08-11 12:26:27 +0200

    [#81766] frontend: tests: yaml_editor.spec.ts: Added tests
    Signed-off-by: Anna Roszkiewicz <aroszkiewicz@antmicro.com>

--- pipeline_manager/frontend/tests/yaml_editor.spec.ts
+++ pipeline_manager/frontend/tests/yaml_editor.spec.ts
@@ -1,22 +1,20 @@
-import { test, expect, Page, FileChooser } from '@playwright/test';
-import { setTimeout } from "timers/promises";
-import { getPathToJsonFile, getUrl, addNode, dragAndDrop, enableEditingNodes, loadSpecification, loadDataflow } from './config.js';
 import YAML from 'yaml';
+import {
+    test, expect, Page, FileChooser,
+} from '@playwright/test';
+import {
+    getPathToJsonFile, getUrl, addNode, dragAndDrop, enableEditingNodes,
+    loadSpecification, loadDataflow, openNodePalette,
+} from './config.js';
 
-async function assertInputCount(page: Page, nodeName: string, count: integer) {
+async function assertInputCount(page: Page, nodeName: string, count: number) {
     const inputs = await page
         .locator(`[data-node-type="${nodeName}"]`)
         .locator('.__interfaces .__inputs > div')
         .count();
     expect(inputs).toBe(count);
 }
-async function openNodePalette(page: Page) {
-    // Assert that node types can be added.
-    await page.locator('div').filter({ hasText: /^Show node browser$/ }).first().click();
-    const nodePalette = page.locator('.baklava-node-palette');
-    const addNodeButton = nodePalette.getByText('New Node Type').first();
-    expect(addNodeButton).toBeVisible();
-}
+
 async function createNewNodeType(page: Page) {
     // Open node configuration menu
     const nodePalette = page.locator('.baklava-node-palette');
@@ -42,17 +40,6 @@ async function addInterface(page: Page, nodeName: string) {
     await page.getByRole('button', { name: 'Add interface' }).click();
 
     await assertInputCount(page, nodeName, 2);
-}
-
-async function openFileChooser(
-    page: Page,
-    purpose: 'specification' | 'dataflow',
-): Promise<FileChooser> {
-    const fileChooserPromise = page.waitForEvent('filechooser');
-    await page.mouse.move(25, 25);
-    const text = purpose === 'specification' ? 'Load specification' : 'Load graph file';
-    await page.getByText(text).click();
-    return await fileChooserPromise;
 }
 
 async function checkIfYAMLPersists(page: Page) {
@@ -120,4 +107,57 @@ test('adding interface from UI reflected in YAML editor', async ({ page }) => {
 
     expect(initialInterfacesCount).toBe(0);
     expect(modifiedInterfacesCount).toBe(1);
+});
+test('adding interface to YAML', async ({ page }) => {
+    await page.goto(getUrl());
+    await loadSpecification(page, 'sample-include-specification.json');
+    await addNode(page, 'Filesystem', 'LoadVideo', 750, 80);
+});
+test('adding property to YAML', async ({ page }) => {
+    await page.goto(getUrl());
+    await loadSpecification(page, 'sample-include-specification.json');
+});
+test('rename node type', async ({ page }) => {
+    await page.goto(getUrl());
+
+    await loadSpecification(page, 'sample-include-specification.json');
+
+    await addNode(page, 'Filesystem', 'LoadVideo', 750, 80);
+    const node = page.getByText('LoadVideo').locator('..').last();
+    await node.dblclick();
+
+    const textarea = page.locator('textarea');
+    const nodeSpecification = await textarea.inputValue();
+    const newSpecification = nodeSpecification.replace('LoadVideo', 'New node name');
+
+    await textarea.fill(newSpecification);
+    await page.getByText('Apply', { exact: true }).click();
+
+    // assert that both nodes are renamed
+    const editedNode = page.locator('[data-node-type="New node name"]');
+    expect(await editedNode.count()).toBe(2);
+
+    // assert that the node is renamed in node palette
+    const nodePalette = page.locator('.baklava-node-palette');
+    const category = nodePalette.getByText('Filesystem');
+
+    await openNodePalette(page);
+    await category.scrollIntoViewIfNeeded();
+    await expect(category).toBeVisible();
+
+    const newNodeEntry = nodePalette.getByText('New node name');
+    expect(newNodeEntry).toBeVisible();
+    const oldNodeEntry = nodePalette.getByText('LoadVideo', { exact: true });
+    expect(oldNodeEntry).not.toBeAttached();
+
+    // assert that both nodes have inherited properties
+    expect(editedNode.first().getByText('filename')).toBeVisible();
+    expect(editedNode.first().getByText('frames')).toBeVisible();
+    expect(editedNode.nth(1).getByText('filename')).toBeVisible();
+    expect(editedNode.nth(1).getByText('frames')).toBeVisible();
+
+    // assert that a new node has inherited properties
+    await dragAndDrop(page, newNodeEntry, 300, 300);
+    expect(editedNode.nth(2).getByText('filename')).toBeVisible();
+    expect(editedNode.nth(2).getByText('frames')).toBeVisible();
 });
