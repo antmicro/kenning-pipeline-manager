0f5f1a038eadc87449d72847ee1c148c9331f116
Anna Roszkiewicz <aroszkiewicz@antmicro.com>
2025-08-21 15:11:34 +0200

    [#82252] frontend: src: icons: Sidebar.vue: Add hover effect
    Signed-off-by: Anna Roszkiewicz <aroszkiewicz@antmicro.com>

--- pipeline_manager/frontend/src/icons/Sidebar.vue
+++ pipeline_manager/frontend/src/icons/Sidebar.vue
@@ -10,7 +10,9 @@ SPDX-License-Identifier: Apache-2.0
         <g clip-path="url(#clip0_1450_188)">
             <path
                 d="M9 19V13H11V15H19V17H11V19H9ZM1 17V15H7V17H1ZM5 13V11H1V9H5V7H7V13H5ZM9 11V9H19V11H9ZM13 7V1H15V3H19V5H15V7H13ZM1 5V3H11V5H1Z"
-                fill="#E5E5E5" />
+                fill="#E5E5E5"
+                :class="hoverStatus"
+            />
         </g>
         <defs>
             <clipPath id="clip0_1450_188">
@@ -19,3 +21,34 @@ SPDX-License-Identifier: Apache-2.0
         </defs>
     </svg>
 </template>
+
+<script>
+import { computed } from 'vue';
+
+export default {
+    props: {
+        hover: {
+            type: Boolean,
+            required: false,
+        },
+    },
+    setup(props) {
+        const hoverStatus = computed(() => ({
+            hovered: props.hover,
+            normal: !props.hover,
+        }));
+
+        return { hoverStatus };
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+.normal{
+    fill: #FFFFFF;
+}
+
+.hovered {
+    fill: $green;
+}
+</style>
