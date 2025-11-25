4a2e695194bfbe906c6087474aa5c0c48f458049
Jan Tacakiewicz <jtacakiewicz@internships.antmicro.com>
2025-11-25 11:32:47 +0100

    [#83386] frontend: tests: yaml_editor: added test checking UI and YAML editor sync

--- pipeline_manager/frontend/tests/yaml_editor.spec.ts
+++ pipeline_manager/frontend/tests/yaml_editor.spec.ts
@@ -1,5 +1,46 @@
 import { test, expect, Page, FileChooser } from '@playwright/test';
-import { getPathToJsonFile, getUrl, enableEditingNodes, loadSpecification, loadDataflow } from './config.js';
+import { setTimeout } from "timers/promises";
+import { getPathToJsonFile, getUrl, addNode, dragAndDrop, enableEditingNodes, loadSpecification, loadDataflow } from './config.js';
+import YAML from 'yaml';
+
+async function assertInputCount(page: Page, nodeName: string, count: integer) {
+    const inputs = await page
+        .locator(`[data-node-type="${nodeName}"]`)
+        .locator('.__interfaces .__inputs > div')
+        .count();
+    expect(inputs).toBe(count);
+}
+async function openNodePalette(page: Page) {
+    // Assert that node types can be added.
+    await page.locator('div').filter({ hasText: /^Show node browser$/ }).first().click();
+    const nodePalette = page.locator('.baklava-node-palette');
+    const addNodeButton = nodePalette.getByText('New Node Type').first();
+    expect(addNodeButton).toBeVisible();
+}
+async function createNewNodeType(page: Page) {
+    // Open node configuration menu
+    const nodePalette = page.locator('.baklava-node-palette');
+    const addNodeButton = nodePalette.getByText('New Node Type').first();
+    await dragAndDrop(page, addNodeButton, 750, 80);
+
+    // Create node
+    const nodeMenu = page.locator('#container').locator('.create-menu');
+    const createButton = nodeMenu.getByText('Create');
+    await createButton.click();
+}
+async function getYAMLEditorContent(page: Page) {
+    const textarea = page.locator('textarea');
+    const value = await textarea.evaluate((el) => el.value);
+    return value;
+}
+async function addInterface(page: Page, nodeName: string) {
+    const node = page.getByText(nodeName).last();
+    await node.click({ button: 'right', force: true });
+    await page.getByText('Add interface').click();
+    await page.getByRole('button', { name: 'Add interface' }).click();
+
+    await assertInputCount(page, nodeName, 2);
+}
 
 async function openFileChooser(
     page: Page,
@@ -45,4 +86,38 @@ test('create new node type', async ({ page }) => {
 
     await enableEditingNodes(page);
     await checkIfYAMLPersists(page);
+});
+
+test('adding interface from UI reflected in YAML editor', async ({page}) => {
+    await page.goto(getUrl());
+    await loadSpecification(page, 'sample-subgraph-specification.json');
+    await loadDataflow(page, 'sample-subgraph-dataflow.json');
+
+    await enableEditingNodes(page);
+
+    // Insatiate a new node.
+    const nodeName = 'Custom Node';
+    await openNodePalette(page);
+    await createNewNodeType(page);
+    await addNode(page, 'Default category', nodeName, 750, 80);
+
+    // Double click the node to open the YAML editor.
+    const customNode = page.locator('[data-node-type="Custom Node"]').first();
+    await customNode.dblclick({ force: true });
+
+    // Retrieve the initial content of the YAML editor.
+    const initialContent = await getYAMLEditorContent(page);
+    const parsedContent = YAML.parse(initialContent);
+
+    // Retrieve the modified content of the YAML editor.
+    await addInterface(page, nodeName);
+    const modifiedContent = await getYAMLEditorContent(page);
+
+    // Count the number of elements in the `interfaces` attribute.
+    const initialInterfacesCount = parsedContent.interfaces.length;
+    const modifiedParsedContent = YAML.parse(modifiedContent);
+    const modifiedInterfacesCount = modifiedParsedContent.interfaces.length;
+
+    expect(initialInterfacesCount).toBe(0);
+    expect(modifiedInterfacesCount).toBe(1);
 });
