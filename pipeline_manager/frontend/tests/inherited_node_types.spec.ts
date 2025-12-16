d5fc859f27e0e1d56be05f74a9ca6ba460c3950d
Jan Tacakiewicz <jtacakiewicz@internships.antmicro.com>
2025-12-16 12:46:22 +0100

    [#88203] frontend: tests: inherited_nodes: added test that overrides inherited interfaces to see if that has desired effect in the editor

--- pipeline_manager/frontend/tests/inherited_node_types.spec.ts
+++ pipeline_manager/frontend/tests/inherited_node_types.spec.ts
@@ -77,3 +77,38 @@ test('checking renamed inherited property', async ({ page }) => {
         .locator('.__content > .__properties > div');
     expect(await nodeBproperties.count()).toBe(2);
 });
+test('override interface', async ({ page }) => {
+    await page.goto(getUrl());
+    await loadSpecification(page, 'sample-inheritance-specification.json');
+    await loadDataflow(page, 'sample-inheritance-dataflow.json');
+
+    await enableEditingNodes(page);
+
+    const nodeB = page.locator('[data-node-type="Type B"]')
+        .locator('.__title').first();
+    await nodeB.dblclick();
+
+    const initialContent = await getYAMLEditorContent(page);
+    const parsedContent = YAML.parse(initialContent);
+    expect(parsedContent.interfaces.length).toBe(1);
+    parsedContent.interfaces.push(Object.fromEntries([
+        ['name', 'output-a'],
+        ['type', 'Interface'],
+        ['direction', 'inout'],
+        ['override', true],
+        ['side', 'right'],
+    ]));
+    await setYAMLEditorContent(page, YAML.stringify(parsedContent));
+    await page.locator('.__validate-button').click();
+    const rightOutputs = page.locator('[data-node-type="Type B"]')
+        .locator('.__content > .__interfaces > .__outputs > div');
+    expect(await rightOutputs.count()).toBe(2);
+
+    const editedContent = await getYAMLEditorContent(page);
+    const editedParsedContent = YAML.parse(editedContent);
+    expect(editedParsedContent.interfaces.length).toBe(2);
+    editedParsedContent.interfaces.pop();
+    await setYAMLEditorContent(page, YAML.stringify(editedParsedContent));
+    await page.locator('.__validate-button').click();
+    expect(await rightOutputs.count()).toBe(1);
+});
