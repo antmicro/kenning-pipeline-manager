6ca4baeacad2e848bee25dff0e342c051a9867a6
Jan Tacakiewicz <jtacakiewicz@internships.antmicro.com>
2026-02-18 15:34:20 +0100

    [#91777] frontend: tests: saving_loading: corrected tests to run in parallel without collisions

--- pipeline_manager/frontend/tests/saving_loading.spec.ts
+++ pipeline_manager/frontend/tests/saving_loading.spec.ts
@@ -1,68 +1,83 @@
-import os from 'os';
 import fs from 'fs/promises';
 import {
     test, expect, Page,
 } from '@playwright/test';
 import {
-    getUrl, loadSpecification, loadDataflow, openFileChooser, getPathToJsonFile,
+    getUrl,
+    loadSpecification,
+    loadDataflow,
+    openFileChooser,
+    getPathToJsonFile,
 } from './config.js';
 
-const temporaryDir = `${os.tmpdir()}/`;
-
 async function expectNoErrors(page: Page) {
-    const notifications = page.locator('.notifications > .panel > ul > *:not(:has(.info))');
-    const c = await notifications.count();
-    expect(c).toBe(0);
+    const notifications = page.locator(
+        '.notifications > .panel > ul > *:not(:has(.info))',
+    );
+    const count = await notifications.count();
+    expect(count).toBe(0);
 }
+
 async function saveFileAs(
     page: Page,
-    purpose: string,
+    testInfo,
+    purpose: 'specification' | 'dataflow',
     filenameWithoutExtension: string,
 ): Promise<string> {
     const text = (purpose === 'specification' ? 'Save specification as...' : 'Save graph as file as...');
     const logo = page.locator('.logo');
     await logo.hover();
-    const saveAsMenuOption = page.getByRole('button', { name: text });
-    await saveAsMenuOption.click();
 
-    await page.getByPlaceholder('File name').first().fill(filenameWithoutExtension);
-    const saveAsButton = page.getByRole('button', { name: 'Save' });
+    await page.getByRole('button', { name: text }).click();
+
+    const uniqueName = `${filenameWithoutExtension}-${testInfo.workerIndex}-${Date.now()}`;
+
+    await page.getByPlaceholder('File name').first().fill(uniqueName);
 
     const downloadPromise = page.waitForEvent('download');
-    await saveAsButton.click();
+    await page.getByRole('button', { name: 'Save' }).click();
     const download = await downloadPromise;
 
-    const downloadedFilePath = temporaryDir + download.suggestedFilename();
-    await download.saveAs(downloadedFilePath);
+    const filePath = testInfo.outputPath(download.suggestedFilename());
+    await download.saveAs(filePath);
 
-    return downloadedFilePath;
+    return filePath;
 }
-async function saveSpecificationAs(page: Page, filenameWithoutExtension: string): Promise<string> {
-    return saveFileAs(page, 'specification', filenameWithoutExtension);
+
+async function saveSpecificationAs(page: Page, testInfo, filename: string) {
+    return saveFileAs(page, testInfo, 'specification', filename);
 }
-async function saveDataflowAs(page: Page, filenameWithoutExtension: string): Promise<string> {
-    return saveFileAs(page, 'dataflow', filenameWithoutExtension);
+
+async function saveDataflowAs(page: Page, testInfo, filename: string) {
+    return saveFileAs(page, testInfo, 'dataflow', filename);
 }
-async function loadIncludeSpecification(page: Page) {
+
+async function loadIncludeSpecification(page: Page, testInfo) {
     const fileChooser = await openFileChooser(page, 'specification');
     const specificationName = 'sample-include-specification.json';
-    const specification = await fs.readFile(getPathToJsonFile(specificationName), { encoding: 'utf-8' });
+    const specification = await fs.readFile(
+        getPathToJsonFile(specificationName),
+        { encoding: 'utf-8' },
+    );
     const newSpecification = specification.replaceAll(
         'https://raw.githubusercontent.com/antmicro/kenning-pipeline-manager/main/examples/',
-        `http://localhost:7001/`,
+        'http://localhost:7001/',
     );
-    const newSpecificationPath = temporaryDir + specificationName;
+    const newSpecificationPath = testInfo.outputPath(specificationName);
     await fs.writeFile(newSpecificationPath, newSpecification);
     await fileChooser.setFiles(newSpecificationPath);
 }
+
 async function loadSpecificationFromFile(page: Page, specificationFile: string) {
     const fileChooser = await openFileChooser(page, 'specification');
     await fileChooser.setFiles(specificationFile);
 }
+
 async function loadDatflowFromFile(page: Page, dataflowFile: string) {
     const fileChooser = await openFileChooser(page, 'dataflow');
     await fileChooser.setFiles(dataflowFile);
 }
+
 const examples = [
     { specification: 'sample-specification.json', dataflow: 'sample-dataflow.json' },
     { specification: 'sample-include-specification.json', dataflow: 'sample-include-dataflow.json' },
@@ -76,28 +91,24 @@ const examples = [
     { specification: 'sample-related-graph-specification.json', dataflow: undefined },
     { specification: 'sample-dynamic-interfaces-specification.json', dataflow: 'sample-dynamic-interfaces-dataflow.json' },
 ];
-async function openNodePalette(page: Page) {
-    // Assert that node types can be added.
-    await page.locator('div').filter({ hasText: /^Show node browser$/ }).first().click();
-}
+
 examples.forEach(({ dataflow, specification }) => {
-    test(`save and load ${specification}`, async ({ page }) => {
+    test(`save and load ${specification}`, async ({ page }, testInfo) => {
         await page.goto(getUrl());
         if (specification === 'sample-include-specification.json') {
-            await loadIncludeSpecification(page);
+            await loadIncludeSpecification(page, testInfo);
         } else {
             await loadSpecification(page, specification);
         }
-        await openNodePalette(page);
         await expectNoErrors(page);
-        const filepath = await saveSpecificationAs(page, 'temp');
+        const filepath = await saveSpecificationAs(page, testInfo, 'temp');
         await expectNoErrors(page);
         await loadSpecificationFromFile(page, filepath);
         await expectNoErrors(page);
     });
-    if (dataflow === undefined) {
-        return;
-    }
+
+    if (!dataflow) return;
+
     test(`loading ${dataflow}, ${specification}`, async ({ page }) => {
         await page.goto(getUrl());
         await loadSpecification(page, specification);
@@ -105,13 +116,14 @@ examples.forEach(({ dataflow, specification }) => {
         await loadDataflow(page, dataflow);
         await expectNoErrors(page);
     });
-    test(`save and load ${dataflow} using ${specification}`, async ({ page }) => {
+
+    test(`save and load ${dataflow} using ${specification}`, async ({ page }, testInfo) => {
         await page.goto(getUrl());
         await loadSpecification(page, specification);
         await expectNoErrors(page);
         await loadDataflow(page, dataflow);
         await expectNoErrors(page);
-        const filepath = await saveDataflowAs(page, 'temp');
+        const filepath = await saveDataflowAs(page, testInfo, 'temp');
         await expectNoErrors(page);
         await loadDatflowFromFile(page, filepath);
         await expectNoErrors(page);
