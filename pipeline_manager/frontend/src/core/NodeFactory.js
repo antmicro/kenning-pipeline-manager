c0da9899fe62e84e9dce7dd05fa5ea858d74fb15
Patryk Peronski <pperonski@internships.antmicro.com>
2026-03-17 15:03:42 +0100

    [#89160] frontend: core: node_factory: load function set node position when it is available.

--- pipeline_manager/frontend/src/core/NodeFactory.js
+++ pipeline_manager/frontend/src/core/NodeFactory.js
@@ -1063,9 +1063,7 @@ export class CustomNode extends Node {
 
         // Default position should be undefined instead of (0, 0) so that it can be set
         // by autolayout
-        if (state.position === undefined) {
-            this.position = undefined;
-        }
+        this.position = state?.position;
 
         this.relatedGraphs = state.relatedGraphs;
         return errors;
