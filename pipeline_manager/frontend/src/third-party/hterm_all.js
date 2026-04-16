66fb7b49fbd2e3bb678253c083bc950a2642859d
Bartosz Matysiak <bmatysiak@antmicro.com>
2026-04-16 12:52:00 +0200

    [#95167] frontend: third-party: hterm_all: Checking fix for firefox
    Signed-off-by: Bartosz Matysiak <bmatysiak@antmicro.com>

--- pipeline_manager/frontend/src/third-party/hterm_all.js
+++ pipeline_manager/frontend/src/third-party/hterm_all.js
@@ -12643,7 +12643,7 @@ hterm.ScrollPort.prototype.decorate = function(div, callback) {
   // Insert Iframe content asynchronously in FF.  Otherwise when the frame's
   // load event fires in FF it clears out the content of the iframe.
   if ('mozInnerScreenX' in window) { // detect a FF only property
-    this.iframe_.addEventListener('load', () => onLoad());
+    onLoad();
   } else {
     onLoad();
   }
