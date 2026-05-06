b25f280c7b593ca049cb13a56c844a43500c3f3b
Patryk Peronski <pperonski@internships.antmicro.com>
2026-05-06 15:56:22 +0200

    [#90961] backend: config: Added config file storing common global variable used by backend.

---
+++ pipeline_manager/backend/config.py
@@ -0,0 +1,12 @@
+# Copyright (c) 2026 Antmicro <www.antmicro.com>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+"""
+Provides global variables for communication
+backend.
+"""
+
+MAX_CHUNK_SIZE = 20 * 1024
+
+MAX_HTTP_BUFFER_SIZE = MAX_CHUNK_SIZE * 2
