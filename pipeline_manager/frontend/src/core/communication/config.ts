0b87d3f678182d0c6f1ffede80b91e5d3f3f6853
Patryk Peronski <pperonski@internships.antmicro.com>
2026-05-06 16:21:45 +0200

    [#90961] frontend: core: communication: config: Added a new config file which holds global variable used for communication.

---
+++ pipeline_manager/frontend/src/core/communication/config.ts
@@ -0,0 +1,7 @@
+/*
+ * Copyright (c) 2026 Antmicro <www.antmicro.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+// eslint-disable-next-line import/prefer-default-export
+export const MAX_CHUNK_SIZE:number = 20 * 1024;
