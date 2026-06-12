b9b6ecfa459f349a324410d3b12ed4186cc8afa3
Patryk Peronski <pperonski@internships.antmicro.com>
2026-06-12 14:36:36 +0200

    [#95635] frontend: core: palette: spec_graph: Added spec graph for showing graph in specification.

---
+++ pipeline_manager/frontend/src/core/palette/spec_graph.ts
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2026 Antmicro <www.antmicro.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/*
+ * Module contains graph palette entries constructor.
+ */
+
+import { type Graph, useViewModel } from 'baklavajs';
+import {
+    computed,
+    ref,
+    type Reactive,
+    type Ref,
+} from 'vue';
+import PipelineManagerEditor from '../../custom/Editor';
+import {
+    type CustomGraph,
+    type CustomNodeTypeInformation,
+    type CustomViewModel,
+    type IEntry,
+    type IVEntry,
+} from './types';
+import usePalette from './base';
+
+export default function useSpecGraphPalette(
+    nameFilterRef: Ref<string>,
+): Reactive<IEntry[]> {
+    const { viewModel } = useViewModel();
+
+    const gatherGraphs = (
+        graphs: any[],
+        showIds: boolean,
+        visited = new Set<Graph>(),
+    ): IVEntry[] => {
+        if (graphs === undefined) {
+            return [];
+        }
+
+        return graphs.flatMap((graph) => {
+            if (visited.has(graph)) return [];
+            visited.add(graph);
+
+            const specGraph = graph.spec;
+
+            // Title
+            const titleName = [specGraph.name, specGraph.graphNode?.title, 'Anonymous'].find((title) => title && title !== '');
+            const titleId = showIds ? `(${specGraph.id})` : '';
+            const title = `${titleName} ${titleId}`;
+
+            const computedValue = computed(() => ({
+                active: viewModel.value.displayedGraph === graph,
+            }));
+
+            const baseEntry = {
+                id: specGraph.id,
+                data: {
+                    title,
+                    onClick: () => console.log('Switched'),
+                },
+                computed: computedValue,
+            };
+
+            return [baseEntry];
+        });
+    };
+
+    const entries = computed(() => gatherGraphs(
+        (viewModel.value.editor as unknown as PipelineManagerEditor).preloadedGraphs,
+        Boolean((viewModel.value as CustomViewModel).settings.showIds),
+    ));
+    return usePalette(entries, nameFilterRef, undefined, true);
+}
