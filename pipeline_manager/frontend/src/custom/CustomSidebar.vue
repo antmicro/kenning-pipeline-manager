4eece2b81adeb4ece81abd98ec7946b2e2cc897a
Patryk Peronski <pperonski@internships.antmicro.com>
2026-08-20 12:11:27 +0200

    [#103459] frontend: custom: custom_sidebar: updateDynamicInterfaces not expect proxy objects.

--- pipeline_manager/frontend/src/custom/CustomSidebar.vue
+++ pipeline_manager/frontend/src/custom/CustomSidebar.vue
@@ -292,7 +292,7 @@ export default defineComponent({
 
         const updateDynamicInterfaces = (intf) => {
             const nodeIntf = graph.value.nodes.find((x) => x.id === intf.nodeId);
-            nodeIntf?.value.updateDynamicInterfaces(intf);
+            nodeIntf?.updateDynamicInterfaces(intf);
         };
 
         const onPointerLeave = () => {
