b0f3d6fbe9e7c30be4995304338ec26884a49476
Bartosz Matysiak <bmatysiak@antmicro.com>
2026-08-26 12:37:31 +0200

    [#102974] frontend: src: core: Adjust node side in direction of shift taken
    Signed-off-by: Bartosz Matysiak <bmatysiak@antmicro.com>

--- pipeline_manager/frontend/src/core/ConnectionRenderer.js
+++ pipeline_manager/frontend/src/core/ConnectionRenderer.js
@@ -730,8 +730,6 @@ export default class ConnectionRenderer {
             const fromNodeWidth = document.getElementById(fromNode.id).offsetWidth;
             const toNode = graph.nodes.filter((node) => node.id === nc.to.nodeId)[0];
             const toNodeWidth = document.getElementById(toNode.id).offsetWidth;
-            const direction = nc.from.side;
-            const xShift = direction === 'right' ? shift : -shift;
 
             const fromPoints = [
                 {
@@ -761,7 +759,7 @@ export default class ConnectionRenderer {
                 fromPoints,
                 toPoints,
                 regGridStep,
-                xShift,
+                shift,
                 minMargin,
                 nodesInfo,
                 zoneInfo,
